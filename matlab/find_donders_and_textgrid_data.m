ca9917547b00da8dba96f33a52724a67f2e6209b
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2013-10-11 11:33:29 +0200

    documentation updated and some functional changes (because for me it did not work with the regexp

--- matlab/find_donders_and_textgrid_data.m
+++ matlab/find_donders_and_textgrid_data.m
@@ -1,14 +1,18 @@
 function [donders_data, textgrid_data ] = find_donders_and_textgrid_data( audiofile_path )
-% FIND_TEXTGRID_AND_DONDERS_DATA 
-% finds the .textgrid and .donders files for a specific audio file
 
+% FIND_TEXTGRID_AND_DONDERS_DATA helper function to create the filenames
+% for a specified audio file, including the path
+%
+% Example use:
+%   [filename_d, filename_t] = find_donders_and_textgrid_data('fn001055');
 
 base_name = '/home/language/jansch/projects/streams/audio/';
 
-filename = regexp('fn[0-9]+', audiofile_path, 'match');
+%filename = regexp('fn[0-9]+', audiofile_path, 'match');
+filename = audiofile_path;
 
-textgrid_data = fullfile(base_name, filename, '.TextGrid');
-donders_data = fullfile(base_name, filename, '.donders');
+textgrid_data = fullfile(base_name, filename, [filename, '.TextGrid']);
+donders_data  = fullfile(base_name, filename, [filename, '.donders']);
 
 end
 
