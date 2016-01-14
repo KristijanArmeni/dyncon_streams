0f1bafe224f62336399255905073412c3d416588
Kristijan Armeni <kriarm@mentat002.dccn.nl>
2016-01-14 12:26:25 +0100

    Could be that this one had some modifications along the way (not from my part)

--- matlab/streams_statfun_mutualinformation_shift.m
+++ matlab/streams_statfun_mutualinformation_shift.m
@@ -8,7 +8,22 @@ for k = 1:numel(lag)
   shift  = lag(k);
   %disp(shift)
   
-  [fdat, tmpdat] = streams_long2units(design(:,(N+1):(M-N)), dat(:,(shift+N+1):(M+shift-N)), 0);
+  % at this point we can either condense the data matrices such that each
+  % column represents the value for a single word, or we keep the
+  % individual time points
+  if cfg.avgwords
+    [fdat, tmpdat] = streams_long2units(design(:,(N+1):(M-N)), dat(:,(shift+N+1):(M+shift-N)), 0);
+  else
+    % shift in time  
+    fdat   =  design(:,(N+1):(M-N));
+    tmpdat =  dat(:,(shift+N+1):(M+shift-N));
+    
+    % select the non-nans
+    sel        = isfinite(fdat);
+    fdat       = fdat(sel);
+    tmpdat     = tmpdat(:,sel);
+   
+  end
   
   s(:,k) = statfun_mutualinformation(cfg, tmpdat, fdat);
 end
