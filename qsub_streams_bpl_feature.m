0497320734a1fd6c8319d01fb7348052687f68e1
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2016-03-11 18:09:59 +0100

    deleted some unnecessary bits due to reading the data from disk

--- qsub_streams_bpl_feature.m
+++ qsub_streams_bpl_feature.m
@@ -1,37 +1,26 @@
-function qsub_streams_bpl_feature(subject, comps, fband)
+function qsub_streams_bpl_feature(subject, data, featuredata)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-
 %defne the necessary paths for data, features and components
-data_dir = '/home/language/kriarm/matlab/streams_output/data_preproc';
+data_dir = '/home/language/kriarm/streams/streams_output/data_preproc/';
 out_dir = '/home/language/kriarm/matlab/streams_output/stats/meg_model_MI_noDss';
 comp_dir = '/home/language/kriarm/matlab/streams_output/dss_timelocked';
 
 paths = {data_dir, out_dir, comp_dir};
-
-% construct frequency band tag for saving
-freq1 = int2str(fband(1));
-freq2 = int2str(fband(2));
-fname = [freq1 '_' freq2];
-
-% construct relevant file name, feature name and filename for saving
-featuredata = [subject.name '_mdat.mat'];
 feature = 'entropy';
-savefile = [subject.name '_MI_' feature(1:3) '_' fname '_noDss.mat'];
+                            
+[stat] = streams_bpl_feature(subject, data, featuredata, ...
+                            'paths', paths, ... 
+                            'feature', feature, ...
+                            'lag', (-30:3:30), ...
+                            'method', 'mi', ...
+                            'nshuffle', 50);
 
-[~, data, ~] = streams_preproc(subject,...
-                                'ramp', 'up',...
-                                'bpfreq', fband, ...
-                                'audiofile',subject.audiofile(:));
-
-streams_bpl_feature(subject, data, featuredata, ...
-                    'paths', paths, ... 
-                    'feature', feature, ...
-                    'lag', (-300:10:300), ...
-                    'method', 'mi', ...
-                    'savefile', savefile, ...
-                    'lpfreq', fband(1)./4);
+savename = data;
+savename(14:17) = feature(1:4);
+fullname = fullfile(out_dir, savename);
+save(fullname, 'stat');
 
 end
 
