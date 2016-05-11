89fc3e4480c61067610d4f267ae1a75c4b7c145c
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2016-05-11 10:23:41 +0200

    included s11

--- matlab/streams_subjinfo.m
+++ matlab/streams_subjinfo.m
@@ -217,7 +217,7 @@ switch name
     subject.cac = [0.8 3.6 6 11.2];
   
   case 's11' % This is the first subject in the newly acquired dataset. Behavioral Presentation log for this subject was s01-streams.log
-    subject.dataset   = fullfile(subject.datadir, ['301104402kriarms01' '_1200hz_20160510_01.ds']);
+    subject.dataset   = fullfile(subject.datadir, ['301104402kriarm' name '_1200hz_20160510_01.ds']);
     subject.audiofile = {fullfile(subject.audiodir, 'fn001078.wav');
         fullfile(subject.audiodir, 'fn001055.wav')
         fullfile(subject.audiodir, 'fn001293.wav')
