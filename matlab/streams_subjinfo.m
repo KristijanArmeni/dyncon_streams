29d398ffccb7ac8c5f8aedf95a3f08d56db47347
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2016-09-30 10:38:49 +0200

    added blocks for s24 and s25 and some typo corrections

--- matlab/streams_subjinfo.m
+++ matlab/streams_subjinfo.m
@@ -383,7 +383,19 @@ switch name
     subject.eogv.badcomps = [];
     subject.montage.labelorg = {'EEG057';'EEG058';'EEG059'};
     subject.montage.labelnew = {'ECG';  'EOGv';  'EOGh'};
-
+    
+  case 's25'
+    subject.dataset = fullfile(subject.datadir, ['301104402kriarm' name '_1200hz_20160926_01.ds']);
+          subject.audiofile = {fullfile(subject.audiodir, 'fn001078.wav');
+          fullfile(subject.audiodir, 'fn001155.wav')
+          fullfile(subject.audiodir, 'fn001293.wav')
+          fullfile(subject.audiodir, 'fn001443.wav')
+          fullfile(subject.audiodir, 'fn001498.wav')};
+    subject.id = '111865';
+    subject.eogv.badcomps = [];
+    subject.montage.labelorg = {'EEG057';'EEG058';'EEG059'};
+    subject.montage.labelnew = {'ECG';  'EOGv';  'EOGh'};
+    
 end
 
 if ~strcmp(name, 's01')
