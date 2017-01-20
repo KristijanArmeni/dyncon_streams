50c11c9f5d48f5f9ea0f3bfe5201989851a4be32
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-01-20 18:24:25 +0100

    adjusted and made simpler for the same treatment of audio and language data structures

--- matlab/streams_statstruct.m
+++ matlab/streams_statstruct.m
@@ -1,4 +1,4 @@
-function [miAc, miReal, miShuf, miRand, filename] = streams_statstruct(datadir, filename_part, varargin)
+function [mi, filename] = streams_statstruct(datadir, filename_part)
 %STREAMS_STATSTRUCT Creates .mat files with datasets per feature and story
 %   It reads contents from the directory specified in datadir string argument and joins separate
 %   datafiles into one .mat structure for use with ft_timelockstatistics
@@ -10,52 +10,26 @@ getdata = fullfile(datadir, filename_part);
 files = dir(getdata);
 files = {files.name}';
 
-% create cell structures for looping over
-miAc = cell(numel(files), 1);
-miReal = cell(numel(files), 1);
-miShuf = cell(numel(files), 1);
-miRand = cell(numel(files), 1);
+fprintf('I found these files:\n')
+disp(files);
+
+mi = cell(numel(files), 1);
+fields2remove = {'statshuf', 'mi'};
 
 % loop over all files and store them to the appropriate .mat structure
-
 for k = 1 : numel(files)
-
-    filename = files{k};
-    load(filename);
+        
+        filename = files{k};
+        load(filename);
     
-    if ~isfield(stat, 'statshuf')
-        
-        % general procedure
-        miAc{k} = stat;
-        
-    elseif isfield(stat, 'statshuf') && isfield(stat, 'statrand')
         % real MI condition
-        miReal{k} = stat;
-        miReal{k} = rmfield(miReal{k}, {'statshuf', 'statrand'});    % remove the statshuf timecourse
+        mi{k} = stat;
+        mi{k}.stat = stat.mi - nanmean(stat.statshuf, 3);
+        mi{k}.raw = stat.mi;
+        mi{k}.shuf = nanmean(stat.statshuf, 3);
         
-        % surrogate MI
-        miShuf{k} = stat;
-        miShuf{k} = rmfield(miShuf{k}, {'statshuf', 'statrand', 'stat'});          % remove old .statshuf, .stat & field
-        miShuf{k}.stat = mean(stat.statshuf, 3);                       % assign .statshuf timecourse as .stat field
-        miShuf{k} = orderfields(miShuf{k}, miReal{k});                 % order fields as in miReal
-        
-        % surrogate rand MI
-        miRand{k} = stat;
-        miRand{k} = rmfield(miRand{k}, {'statshuf', 'statrand', 'stat'});          % remove old .statshuf, statrand, .stat & field
-        miRand{k}.stat = mean(stat.statrand, 3);                       % assign .statrand timecourse as .stat field
-        miRand{k} = orderfields(miRand{k}, miReal{k});                 % order fields as in miReal
-        
-    else
-        % real MI condition
-        miReal{k} = stat;
-        miReal{k} = rmfield(miReal{k}, {'statshuf'});    % remove the statshuf timecourse
-        
-        % surrogate MI
-        miShuf{k} = stat;
-        miShuf{k} = rmfield(miShuf{k}, {'statshuf', 'stat'});          % remove old .statshuf, .stat & field
-        miShuf{k}.stat = mean(stat.statshuf, 3);                       % assign .statshuf timecourse as .stat field
-        miShuf{k} = orderfields(miShuf{k}, miReal{k});                 % order fields as in miReal
-    end
+       
+        mi{k} = rmfield(mi{k}, fields2remove);
     
 end    
 
