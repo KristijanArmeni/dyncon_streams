5e12a0432e471343e1ded140cabd6b4a3ca7d32e
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-01-27 16:43:30 +0100

    updated anatomy path specification, it now reads subject specific datasets for gradiometer information

--- matlab/streams_leadfield.m
+++ matlab/streams_leadfield.m
@@ -9,24 +9,28 @@ end
 
 % directories 
 subject_code                 = subject.name;
-anatomy_dir                  = fullfile('~/pro/streams/data/MRI/preproc'); %just for test, should be: '/home/language/jansch/projects/streams/data/anatomy'
-freesurfer_dir               = fullfile(anatomy_dir, subject_code); % points to the directory with parcellation and labels
-atlas_dir                    = fullfile('~/pro/streams/data/MRI/atlas/374');
+anatomy_dir                  = fullfile('/project/3011044.02/preproc/anatomy');
+meg_dir                      = fullfile('/project/3011044.02/preproc/meg');
+atlas_dir                    = fullfile('/project/3011044.02/preproc/atlas');
 
-% filenames
+% filenames for loading
 headmodel_filename           = fullfile(anatomy_dir, [subject_code, '_headmodel.mat']);
 sourcemodel_filename         = fullfile(anatomy_dir, [subject_code, '_sourcemodel.mat']);
+parcellation                 = fullfile(atlas_dir, '374/atlas_subparc374_8k.mat');
+datafile                     = fullfile(meg_dir, sprintf('%s_1078_01-03_30Hz_meg.mat', subject_code));
+
+%for saving
 leadfield_filename           = fullfile(anatomy_dir, [subject_code, '_leadfield.mat']);
-parcellation                 = fullfile(atlas_dir, 'atlas_subparc374_8k.mat');
 leadfield_parcellated        = fullfile(anatomy_dir, [subject_code, '_leadfield_parc.mat']);
+
+
+%% Full leadfield
 
 % load the necessary files
 load(sourcemodel_filename)
 load(headmodel_filename)
 load(parcellation);  % load in the atlas
-load('~/pro/streams/data/MEG/preproc/s02_fn1001078_04-08_entr_300hz.mat');
-
-%% Full leadfield
+load(datafile); %for gradiometer information
 
 % compute leadfield
 cfg = [];
@@ -35,13 +39,20 @@ cfg.pos = sourcemodel.pos;
 cfg.inside = sourcemodel.inside;
 leadfield = ft_prepare_leadfield(cfg, data);
 
+%% Also save the original leadfield, but with a 'dummy' label, to allow code further downstream to work
+leadfield.labelorg = leadfield.label;
+inside = find(leadfield.inside);
+for k = 1:numel(inside)
+    indx = inside(k);
+    leadfield.label{k} = sprintf('source%0.5d',indx);
+end
+
 save(leadfield_filename, 'leadfield');
 
 %% Parcellated leadfield
 
 leadfield_parc = streams_parcellate_leadfield(leadfield, atlas);
 save(leadfield_parcellated, 'leadfield_parc');
-
 
 end
 
