e7e968d36d1d2e27b3a5351ceb2bbbe5285aefb5
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-02-06 16:55:29 +0100

    renamed from qsub_streams_preproc.m

---
+++ matlab/pipeline_preprocessing_bandpasslimited_qsub.m
@@ -0,0 +1,39 @@
+function pipeline_preprocessing_bandpasslimited_qsub(subject, audiofile, bpfreq)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+savedir = '/project/3011044.02/preproc/meg';
+
+% preprocessing options
+fsample = 150;
+
+lowfreq = sprintf('%02d', bpfreq(1));
+highfreq = sprintf('%02d', bpfreq(2));
+frequency = [lowfreq, '-', highfreq];
+filename = [subject.name '_' audiofile(5:end) '_' frequency '_' [num2str(fsample) 'Hz']];
+
+%% Preprocessing, band-pass filtering, complex hilbert and downsampling
+                        
+[data, audio] = streams_preprocessing(subject, ...
+                            'audiofile', audiofile, ...
+                            'bpfreq', bpfreq, ...
+                            'docomp', 1, ...
+                            'fsample', fsample, ...
+                            'filter_audio', 'yes', ...
+                            'filter_audiobdb', 'yes');
+
+megfilename = fullfile(savedir, [filename '_meg']);
+audiofilename = fullfile(savedir, [filename '_aud']);
+
+% save the pipeline if not yet saved
+if ~exist([megfilename '.html'], 'file')
+    cfgt = [];
+    cfgt.file = megfilename;
+    cfgt.filetype = 'html';
+    out = ft_analysispipeline(cfgt, data);
+end
+    
+save(megfilename, 'data');
+save(audiofilename, 'audio');
+
+end
