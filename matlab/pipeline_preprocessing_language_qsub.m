82c1ddefa88a07c2e8cf90cd17238b5cf39a1513
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-02-09 09:39:08 +0100

    function call and path specification changes

--- matlab/pipeline_preprocessing_language_qsub.m
+++ matlab/pipeline_preprocessing_language_qsub.m
@@ -3,26 +3,28 @@ function pipeline_preprocessing_language_qsub(subject, audiofile)
 savedir = '/project/3011044.02/preproc/language';
 
 % preprocessing options
-fsample = 30;
+fsample = 150;
 features = {'perplexity' 'entropy' 'entropyred' 'depind' 'gra_perpl' 'pho_perpl'};
 filename = [subject.name '_' audiofile(5:end) '_feature_' [num2str(fsample) 'Hz']];
+fullname = fullfile(savedir, filename);
+pipelinefilename = ['s01_1078_feature_' [num2str(fsample) 'Hz']];
 
-
+% preprocess language data
 featuredata = streams_preprocessing_language(subject, ...
                                               'audiofile', audiofile, ...
                                               'feature', features, ...
                                               'fsample', fsample, ...
                                               'addnoise', 0);
 
-
-if ~exist([pipelinefilename '.html'], 'file')
+                               
+% save the info on preprocessing options used
+if ~exist(fullfile(savedir, [pipelinefilename '.html']), 'file')
     cfgt = [];
     cfgt.filename = filename;
     cfgt.filetype = 'html';
-    ft_analysispipeline(cfgt, data);
+    ft_analysispipeline(cfgt, featuredata);
 end
-                                  
-fullname = fullfile(savedir, filename);
+
 save(fullname, 'featuredata')
 
 end
