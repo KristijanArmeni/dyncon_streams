b5dce0b3d2581f105c489e51b3587d21cd7b3809
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-02-15 13:56:43 +0100

    changed audiofile flag to all

--- matlab/pipeline_preprocessing_broadband.m
+++ matlab/pipeline_preprocessing_broadband.m
@@ -10,20 +10,12 @@ subjects = {'s01', 's02', 's03', 's04', 's05', 's07', 's08', 's09', 's10'};
 % MEG: Subject, story and freq loops
 for j = 1:numel(subjects)
 	subject    = streams_subjinfo(subjects{j});
-	audiofiles = subject.audiofile;
-	
-  for k = 1:numel(audiofiles)
-		
-    audiofile = audiofiles{k};
-	tmp       = strfind(audiofile, 'fn');
-	audiofile = audiofile(tmp+(0:7));
-
+    audiofile = 'all';
     
-   qsubfeval('pipeline_preprocessing_broadband_qsub', subject, audiofile, ...
+    qsubfeval('pipeline_preprocessing_broadband_qsub', subject, audiofile, ...
                       'memreq', 1024^3 * 12,...
                       'timreq', 240*60,...
                       'batchid', 'streams_preproc');
     
-  end
 
 end
