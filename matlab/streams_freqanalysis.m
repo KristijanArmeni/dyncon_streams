73409bd184ac709a1d867b5b8833dbc0629fb0eb
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-02-28 11:33:32 +0100

    added the code from pipeline_freqanalysis_qsub.m, this quy now does the job again

--- matlab/streams_freqanalysis.m
+++ matlab/streams_freqanalysis.m
@@ -1,77 +1,69 @@
-function [freq, data, featuredata] = streams_freqanalysis(data, featuredata)
+function [freq, data, featuredata] = streams_freqanalysis(data, featuredata, epochlength)
 %streams_freqanalysis() chunks the data into 1s long epochs and computes
 %powerspectra via ft_freqanalysis
 
 
 %% Epoch the data
 
-epoch = data.fsample; % chunk size in samples
-measure = 'entropy';
-measure_index = find(strcmp(featuredata.label, measure));
+cfg = [];
+cfg.length = epochlength;
+data_epoched = ft_redefinetrial(cfg, data);
 
-datatmp         = [data.trial{:}];
-featuredatatmp  = [featuredata.trial{:}];
-featuredatatmp  = featuredatatmp(measure_index, :); % select the feature value, e.g. entropy
-timetmp         = [data.time{:}];
-end_sample      = size(datatmp, 2);
+cfg = [];
+cfg.length = epochlength;
+featuredata_epoched = ft_redefinetrial(cfg, featuredata);
 
-% check for potential downsampling inconsistencies
-if ~isequal(size(datatmp, 2), size(featuredatatmp, 2))
-    error('MEG trials length are not equal to language vector trial lengths. Please check.')
+
+%% Compute trial information (time information and complexity values)
+data_epoched.trialinfo(:, 2) = cellfun(@(v) v(1), data_epoched.time(:)); % add onset time values for trials
+
+story_offset = abs(diff(data_epoched.trialinfo(:,1))) ~= 0; % find story ending indices and mark as 1
+story_offset(end + 1) = 1; %final index
+
+offset_times = data_epoched.trialinfo(story_offset, 2);
+story_markers = data_epoched.trialinfo(story_offset, 1);
+
+% compute normalized time per story
+story_norm = cell(1, numel(offset_times));
+for i = 1: numel(offset_times)
+    story_norm{i} = data_epoched.trialinfo(data_epoched.trialinfo(:,1) == story_markers(i), 2)./offset_times(i);
 end
 
-num_epochs = floor(end_sample/epoch); % determine number of epochs
-residual = mod(end_sample, epoch);
+% concatenate back to an array
+time_norm = vertcat(story_norm{:});
+data_epoched.trialinfo(:,3) = time_norm;
 
-epochs = zeros(1, num_epochs);
-epochs(:) = epoch;
-
-% epoching
-data_epoched = mat2cell(datatmp, size(datatmp, 1), [epochs, residual]); % chunk the data into a cell array of epoch length
-data_epoched = data_epoched(1:end-1); % drop the last trial which is of shorter time lenght then the rest
-
-featuredata_epoched = mat2cell(featuredatatmp, size(featuredatatmp, 1), [epochs, residual]);
-featuredata_epoched = featuredata_epoched(1:end-1);
-
-time_epoched = mat2cell(timetmp, size(timetmp, 1), [epochs, residual]);
-time_epoched = time_epoched(1:end-1);
-time_epoched(:) = {(0:1:epoch-1)./epoch};
-
-
-%% Do median split on mean language values
-
-trials_mean = cellfun(@nanmean, featuredata_epoched);
-trials_median = nanmedian(trials_mean(:));
-
-trials_grouped = nan(1, numel(trials_mean));
-
-indx_high = find(trials_mean(:) >= trials_median);
-indx_low  = find(trials_mean(:) < trials_median);
-
-trials_grouped(indx_high) = 2;
-trials_grouped(indx_low) = 1;
+% compute mean lang. complexity values
+for k = 1:3
+   
+    cfg = [];
+    cfg.channel = k;
+    tmp = ft_selectdata(cfg, featuredata_epoched);
+    
+    data_epoched.trialinfo(:, k + 3) = cellfun(@nanmean, tmp.trial(:));
+    
+end
 
 
 %% do freqanalysis
-
-dataorig = data;
-featuredataorig = featuredata;
-
-data.trial = data_epoched;
-data.time = time_epoched;
-data.trialinfo = trials_grouped';
-
-featuredata.trial = featuredata_epoched;
-featuredata.time = time_epoched;
-featuredata.label = {measure};
 
 cfg = [];
 cfg.method = 'mtmfft';
 cfg.output = 'pow';
 cfg.taper = 'hanning';
-cfg.pad = 'nextpow2';
+cfg.tapsmofrq = 2;
 cfg.keeptrials = 'yes';
-freq = ft_freqanalysis(cfg, data);
+freq = ft_freqanalysis(cfg, data_epoched);
+
+% add trial information and labels
+freq.trialinfo = data_epoched.trialinfo; % for plotting
+freq.trialinfolabel{1,1} = 'story';
+freq.trialinfolabel{2,1} = 'onsettime';
+freq.trialinfolabel{3,1} = 'time_norm';
+freq.trialinfolabel{4,1} = 'mean_perplexity';
+freq.trialinfolabel{5,1} = 'mean_entropy';
+freq.trialinfolabel{6,1} = 'mean_entropyred';
+
 
 end
 
