ce7f9452a947d3e95485359f4af998022bb305c1
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-02-28 16:11:05 +0100

    trivial, removed unused block of code

--- matlab/pipeline_freqanalysis.m
+++ matlab/pipeline_freqanalysis.m
@@ -1,3 +1,4 @@
+
 clear all
 if ~ft_hastoolbox('qsub',1)
     addpath /home/kriarm/git/fieldtrip/qsub;
@@ -7,23 +8,18 @@ subjects = {'s01', 's02', 's03', 's04', 's05', 's07', 's08', 's09', 's10'};
 
 
 % subject loop
-
 for j = 1:numel(subjects)
-	subject    = streams_subjinfo(subjects{j});
+	
+    
+    subject    = streams_subjinfo(subjects{j});
 	audiofiles = subject.audiofile;
 	audiofile = 'all';
-    
-%   for k = 1:numel(audiofiles)
-		
-%   audiofile = audiofiles{k};
-% 	tmp       = strfind(audiofile, 'fn');
-% 	audiofile = audiofile(tmp+(0:7));
+
     
     qsubfeval('pipeline_freqanalysis_qsub', subject, audiofile, ...
-                      'memreq', 1024^3 * 12,...
-                      'timreq', 240*60,...
-                      'batchid', 'streams_features');
-    
-%   end
+                                                      'memreq', 1024^3 * 12,...
+                                                      'timreq', 240*60,...
+                                                      'batchid', 'streams_features');
+
 
 end
