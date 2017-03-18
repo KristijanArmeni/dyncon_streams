d4b609209fc3db5920c0f7b904760dd3fdd0e54a
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-03-18 17:34:00 +0100

    added the switch syntax for different pipelines, options are specified in optarg

--- matlab/pipeline_preprocessing_broadband.m
+++ matlab/pipeline_preprocessing_broadband.m
@@ -1,21 +1,49 @@
 
 clear all
+
 if ~ft_hastoolbox('qsub',1)
     addpath /home/kriarm/git/fieldtrip/qsub;
 end
 
 subjects = {'s01', 's02', 's03', 's04', 's05', 's07', 's08', 's09', 's10'};
+pipeline = '2';
+
+%% PREPROCESSING PIPELINES
+
+switch pipeline
+    
+    case '1' % 1-100 bandpass, 200Hz downsampling
+    fprintf('Running pipeline Nr %s. \n\n', pipeline);
+    for j = 1:numel(subjects)
+        
+        subject    = streams_subjinfo(subjects{j});
+        audiofile = 'all';
+        optarg = {'lpfreq', 100, 'hpfreq', 1, 'fsample', 200};
+        
+        qsubfeval('pipeline_preprocessing_broadband_qsub', subject, audiofile, optarg, ...
+                          'memreq', 1024^3 * 12,...
+                          'timreq', 240*60,...
+                          'batchid', 'streams_preproc');
 
 
-% MEG: Subject, story and freq loops
-for j = 1:numel(subjects)
-	subject    = streams_subjinfo(subjects{j});
-    audiofile = 'all';
+    end
+   
     
-    qsubfeval('pipeline_preprocessing_broadband_qsub', subject, audiofile, ...
-                      'memreq', 1024^3 * 12,...
-                      'timreq', 240*60,...
-                      'batchid', 'streams_preproc');
+    case '2' % 1-150 bandpass, 300Hz downsampling
+    fprintf('Running pipeline Nr %s \n\n', pipeline);
     
+    for j = 1:numel(subjects)
+        
+        subject    = streams_subjinfo(subjects{j});
+        audiofile = 'all';
+        optarg = {'lpfreq', 150, 'hpfreq', 1, 'fsample', 300};
+        
+        qsubfeval('pipeline_preprocessing_broadband_qsub', subject, audiofile, optarg, ...
+                          'memreq', 1024^3 * 12,...
+                          'timreq', 240*60,...
+                          'batchid', 'streams_preproc');
+
+
+    end
 
 end
--- matlab/pipeline_preprocessing_language.m
+++ matlab/pipeline_preprocessing_language.m
@@ -5,25 +5,38 @@ if ~ft_hastoolbox('qsub',1)
 end
 
 subjects = {'s01', 's02', 's03', 's04', 's05', 's07', 's08', 's09', 's10'};
+pipeline = '2';
 
+switch pipeline
 
-% Language: Subject, story loops
-for j = 1:numel(subjects)
-	subject    = streams_subjinfo(subjects{j});
-	audiofiles = subject.audiofile;
-	audiofile = 'all';
-    
-%   for k = 1:numel(audiofiles)
-		
-%     audiofile = audiofiles{k};
-% 	tmp       = strfind(audiofile, 'fn');
-% 	audiofile = audiofile(tmp+(0:7));
-    
-    qsubfeval('pipeline_preprocessing_language_qsub', subject, audiofile, ...
-                      'memreq', 1024^3 * 12,...
-                      'timreq', 240*60,...
-                      'batchid', 'streams_features');
-    
-%   end
+    case '1' % 200Hz downsampling
+    fprintf('Running pipeline Nr %s \n\n', pipeline);
+    for j = 1:numel(subjects)
 
+        subject    = streams_subjinfo(subjects{j});
+        audiofile = 'all';
+        optarg = {'fsample', 200};
+
+        qsubfeval('pipeline_preprocessing_language_qsub', subject, audiofile, optarg, ...
+                          'memreq', 1024^3 * 12,...
+                          'timreq', 240*60,...
+                          'batchid', 'streams_features');
+
+    end
+    
+    case '2' % 300Hz downsampling
+    fprintf('Running pipeline Nr %s \n\n', pipeline);
+    for j = 1:numel(subjects)
+
+        subject    = streams_subjinfo(subjects{j});
+        audiofile = 'all';
+        optarg = {'fsample', 300, ...
+                  'features', {'nchar' 'log10wf' 'depind' 'perplexity' 'log10perp' 'entropy' 'entropyred'}};
+
+        qsubfeval('pipeline_preprocessing_language_qsub', subject, audiofile, optarg, ...
+                          'memreq', 1024^3 * 3,...
+                          'timreq', 40*60);
+
+    end
+    
 end
