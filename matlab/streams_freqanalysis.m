b242b5747c8ad23ae2c4415c92db3a717710c40f
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-03-18 18:02:20 +0100

    important changes, some post-hoc data cleaning added, created streams_freqanalysis_trialinfo subfunction and conversion to planar data

--- matlab/streams_freqanalysis.m
+++ matlab/streams_freqanalysis.m
@@ -1,4 +1,4 @@
-function [freq, data, featuredata] = streams_freqanalysis(data, featuredata, epochlength)
+function [freq, data, featuredata] = streams_freqanalysis(data, featuredata, epochlength, taper, tapsmooth)
 %streams_freqanalysis() chunks the data into 1s long epochs and computes
 %powerspectra via ft_freqanalysis
 
@@ -7,63 +7,99 @@ function [freq, data, featuredata] = streams_freqanalysis(data, featuredata, epo
 
 cfg = [];
 cfg.length = epochlength;
-data_epoched = ft_redefinetrial(cfg, data);
+data = ft_redefinetrial(cfg, data);
 
 cfg = [];
 cfg.length = epochlength;
-featuredata_epoched = ft_redefinetrial(cfg, featuredata);
+featuredata = ft_redefinetrial(cfg, featuredata);
 
+% use some heuristic to remove trials that, across the channel array, have
+% high variance in the individual epochs
+tmp = ft_channelnormalise([], data);
+S   = cellfun(@std,tmp.trial, repmat({[]},[1 numel(tmp.trial)]), repmat({2},[1 numel(tmp.trial)]), 'uniformoutput', false);
+S   = cat(2,S{:});
 
+sel = find(~(sum(S>2)>=5 | sum(S>3)>0)); % at least five channels for which the individual 
+% trials's STD is exceeding 2, where the value of 2 is the relative STD of that chnnel's trial, relative to the whole dataset
+
+cfg = [];
+cfg.trials = sel;
+data = ft_selectdata(cfg, data);
+featuredata = ft_selectdata(cfg, featuredata);
+clear tmp;
+
+%% Meg planar
+ 
+fprintf('Converting to planar gradients...\n\n')
+
+cfg              = [];
+cfg.feedback     = 'no';
+cfg.method       = 'template';
+cfg.planarmethod = 'sincos';
+cfg.channel      = {'MEG'};
+cfg.trials       = 'all';
+cfg.neighbours   = ft_prepare_neighbours(cfg, data);
+
+data      = ft_megplanar(cfg, data);
+    
 %% Compute trial information (time information and complexity values)
-data_epoched.trialinfo(:, 2) = cellfun(@(v) v(1), data_epoched.time(:)); % add onset time values for trials
 
-story_offset = abs(diff(data_epoched.trialinfo(:,1))) ~= 0; % find story ending indices and mark as 1
-story_offset(end + 1) = 1; %final index
+% add trialinfo
+selected_features = {'nchar' 'log10wf' 'log10perp' 'entropy'};
+featuredata = streams_freqanalsysis_trialinfo(featuredata, selected_features);
 
-offset_times = data_epoched.trialinfo(story_offset, 2);
-story_markers = data_epoched.trialinfo(story_offset, 1);
-
-% compute normalized time per story
-story_norm = cell(1, numel(offset_times));
-for i = 1: numel(offset_times)
-    story_norm{i} = data_epoched.trialinfo(data_epoched.trialinfo(:,1) == story_markers(i), 2)./offset_times(i);
-end
-
-% concatenate back to an array
-time_norm = vertcat(story_norm{:});
-data_epoched.trialinfo(:,3) = time_norm;
-
-% compute mean lang. complexity values
-for k = 1:3
-   
-    cfg = [];
-    cfg.channel = k;
-    tmp = ft_selectdata(cfg, featuredata_epoched);
-    
-    data_epoched.trialinfo(:, k + 3) = cellfun(@nanmean, tmp.trial(:));
-    
-end
-
-
-%% do freqanalysis
+%% do freqanalysis and combine planar if specified
 
 cfg = [];
 cfg.method = 'mtmfft';
 cfg.output = 'pow';
-cfg.taper = 'dpss';
-cfg.tapsmofrq = 2;
+cfg.taper = taper;
+if strcmp(taper, 'dpss'); cfg.tapsmofrq = tapsmooth; end
 cfg.keeptrials = 'yes';
-freq = ft_freqanalysis(cfg, data_epoched);
+freq = ft_freqanalysis(cfg, data);
+
+cfg = [];
+cfg.method = 'sum';
+freq = ft_combineplanar(cfg, freq);
 
 % add trial information and labels
-freq.trialinfo = data_epoched.trialinfo; % for plotting
-freq.trialinfolabel{1,1} = 'story';
-freq.trialinfolabel{2,1} = 'onsettime';
-freq.trialinfolabel{3,1} = 'time_norm';
-freq.trialinfolabel{4,1} = 'mean_perplexity';
-freq.trialinfolabel{5,1} = 'mean_entropy';
-freq.trialinfolabel{6,1} = 'mean_entropyred';
-
+freq.trialinfo = featuredata.trialinfo; % for plotting
+freq.trialinfolabel = featuredata.trialinfolabel;
 
 end
 
+function featuredata = streams_freqanalsysis_trialinfo(featuredata, selected_features)
+
+% data.trialinfo(:, 2) = cellfun(@(v) v(1), data.time(:)); % add onset time values for trials
+
+% story_offset = abs(diff(data.trialinfo(:,1))) ~= 0; % find story ending indices and mark as 1
+% story_offset(end + 1) = 1; %final index
+% 
+% offset_times = data.trialinfo(story_offset, 2);
+% story_markers = data.trialinfo(story_offset, 1);
+% 
+% % compute normalized time per story
+% story_norm = cell(1, numel(offset_times));
+% for i = 1: numel(offset_times)
+%     story_norm{i} = data.trialinfo(data.trialinfo(:,1) == story_markers(i), 2)./offset_times(i);
+% end
+% 
+% % concatenate back to an array
+% time_norm = vertcat(story_norm{:});
+% data.trialinfo(:,3) = time_norm;
+
+% compute mean lang. complexity values
+featuredata.trialinfolabel{1, 1} = 'story'; % this is the preprocessed trialinfo
+for k = 1:numel(selected_features)
+    
+    feature = selected_features{k};
+    chan_indx = find(strcmp(featuredata.label, feature)); % find the correct index
+    
+    tmp = cellfun(@(x) x(chan_indx,:), featuredata.trial(:), 'UniformOutput', 0); % choose the correct row in every cell
+    
+    tmp = cellfun(@unique, tmp(:), 'UniformOutput', 0); % extract unique values
+    featuredata.trialinfo(:, k + 1) = cellfun(@nanmean, tmp(:)); % take the mean, ignoring nans
+    featuredata.trialinfolabel{k + 1, 1} = feature;
+end
+
+end
