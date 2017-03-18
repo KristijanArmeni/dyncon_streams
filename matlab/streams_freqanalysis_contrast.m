4405450021522b7b26d8102edbec21354087632a
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-03-18 18:09:27 +0100

    added ivar_vector variable, added code for indepsamples t-test

--- matlab/streams_freqanalysis_contrast.m
+++ matlab/streams_freqanalysis_contrast.m
@@ -1,16 +1,17 @@
-function [freq_change, freq_high, freq_low] = streams_freqanalysis_contrast(freq, var)
+function [freq_T, freq_high, freq_low] = streams_freqanalysis_contrast(freq, ivar)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-
 % find channel index
-chan_idx = strmatch(var, freq.trialinfolabel(:), 'exact');
+chan_idx = strcmp(freq.trialinfolabel(:), ivar);
+ivar_vector = freq.trialinfo(:, chan_idx); % pick the appropriate language variable
 
-% select the low and high conditions
-threshold_low = prctile(freq.trialinfo(:, chan_idx), 25);
-threshold_high = prctile(freq.trialinfo(:, chan_idx), 75);
-trials_low = freq.trialinfo(:, chan_idx) <= threshold_low;
-trials_high = freq.trialinfo(:, chan_idx) >= threshold_high;
+% select the trials in the low and high quartiles
+threshold_low = prctile(ivar_vector, 25);
+threshold_high = prctile(ivar_vector, 75);
+
+trials_low = ivar_vector <= threshold_low;
+trials_high = ivar_vector >= threshold_high;
 
 cfg = [];
 cfg.trials = trials_low;
@@ -20,20 +21,14 @@ cfg = [];
 cfg.trials = trials_high;
 freq_high = ft_selectdata(cfg, freq);
 
-%%  compute the percent change
+%% compute a t-statistic for the high/low comparison
 
 cfg = [];
-cfg.parameter = 'powspctrm';
-cfg.operation = 'divide';
-freq_change = ft_math(cfg, freq_high, freq_low);
-
-%%  average over trials
-
-cfg = [];
-cfg.avgoverrpt = 'yes';
-freq_change = ft_selectdata(cfg, freq_change);
-freq_high = ft_selectdata(cfg, freq_high);
-freq_low = ft_selectdata(cfg, freq_low);
+cfg.method = 'montecarlo';
+cfg.statistic = 'indepsamplesT'; % for each subject do between trials (independent) t-test
+cfg.numrandomization = 0;
+cfg.design = [ones(1,size(freq_high.trialinfo,1)) ones(1,size(freq_low.trialinfo,1))*2];
+freq_T = ft_freqstatistics(cfg, freq_high, freq_low);
 
 end
 
