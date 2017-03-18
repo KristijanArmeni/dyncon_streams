8bf21bce4ccee2fdb09e774ecf7cf1a920ae5852
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-03-18 17:58:04 +0100

    added switch syntax for better control

--- matlab/pipeline_freqanalysis.m
+++ matlab/pipeline_freqanalysis.m
@@ -4,22 +4,67 @@ if ~ft_hastoolbox('qsub',1)
     addpath /home/kriarm/git/fieldtrip/qsub;
 end
 
-subjects = {'s01', 's02', 's03', 's04', 's05', 's07', 's08', 's09', 's10'};
+subjects = {'s01', 's02', 's03', 's04', 's05', 's07', 's08', 's10'};
+runpipeline = 'hanning';
 
-
-% subject loop
-for j = 1:numel(subjects)
-	
+switch runpipeline
     
-    subject    = streams_subjinfo(subjects{j});
-	audiofiles = subject.audiofile;
-	audiofile = 'all';
+    case 'dpss4'
+    fprintf('Running %s pipeline... \n\n', runpipeline)
+    % subject loop
+    for j = 1:numel(subjects)
+    
+
+        subject    = streams_subjinfo(subjects{j});
+        audiofiles = subject.audiofile;
+        audiofile = 'all';
+
+        optarg = {'filter_range', '01-150', 'sr', '300hz', 'taper', 'dpss', 'tapsmooth', 4, 'epochlength', 1};
+        qsubfeval('pipeline_freqanalysis_qsub', subject, audiofile, optarg, ...
+                                                          'memreq', 1024^3 * 12,...
+                                                          'timreq', 240*60,...
+                                                          'batchid', 'streams_features');
+
+
+    end
 
     
-    qsubfeval('pipeline_freqanalysis_qsub', subject, audiofile, ...
-                                                      'memreq', 1024^3 * 12,...
-                                                      'timreq', 240*60,...
-                                                      'batchid', 'streams_features');
+    case 'dpss8'
+    fprintf('Running %s pipeline... \n\n', runpipeline)
+    % subject loop
+    for j = 1:numel(subjects)
+    
+
+        subject    = streams_subjinfo(subjects{j});
+        audiofiles = subject.audiofile;
+        audiofile = 'all';
+
+        optarg = {'filter_range', '01-150', 'sr', '300hz', 'taper', 'dpss', 'tapsmooth', 8, 'epochlength', 1};
+        qsubfeval('pipeline_freqanalysis_qsub', subject, audiofile, optarg, ...
+                                                          'memreq', 1024^3 * 12,...
+                                                          'timreq', 240*60,...
+                                                          'batchid', 'streams_features');
 
 
+    end
+    
+    case 'hanning'
+    fprintf('Running %s pipeline... \n\n', runpipeline)
+    % subject loop
+    for j = 1:numel(subjects)
+
+
+        subject    = streams_subjinfo(subjects{j});
+        audiofiles = subject.audiofile;
+        audiofile = 'all';
+
+        optarg = {'filter_range', '01-150', 'sr', '300hz', 'taper', 'hanning', 'tapsmooth', [], 'epochlength', 1};
+        qsubfeval('pipeline_freqanalysis_qsub', subject, audiofile, optarg, ...
+                                                          'memreq', 1024^3 * 12,...
+                                                          'timreq', 240*60,...
+                                                          'batchid', 'streams_features');
+
+
+    end
+    
 end
