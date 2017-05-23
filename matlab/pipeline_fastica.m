d5c28dd745c254ba7cdba5b5df9574716c65da5e
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-05-23 12:41:57 +0200

    initial commit, high-level code for looping over subjects

---
+++ matlab/pipeline_fastica.m
@@ -0,0 +1,24 @@
+
+clear all
+
+if ~ft_hastoolbox('qsub',1)
+    addpath /home/kriarm/git/fieldtrip/qsub;
+end
+
+subjects = strsplit(sprintf('s%.2d ', 1:28));
+subjects = subjects(~cellfun(@isempty, subjects));
+
+s6 = strcmp(subjects, 's06');
+subjects(s6) = []; % s06 dataset does not exist, empty it to prevent errors
+
+num_sub = numel(subjects);
+display(subjects);
+
+for i = 1:numel(subjects)
+   
+    subject = subjects{i};
+    qsubfeval('streams_componentanalysis_fastica', subject, ...
+                          'memreq', 1024^3 * 12,...
+                          'timreq', 30*60);
+    
+end
