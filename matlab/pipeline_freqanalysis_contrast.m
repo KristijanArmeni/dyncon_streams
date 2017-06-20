a1d2623d8d5812c644094b8a4d281d75b7f8e9b8
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-06-20 09:33:06 +0200

    added block for running tertile split contrast and a lexfreq contrast (not needed anymore)

--- matlab/pipeline_freqanalysis_contrast.m
+++ matlab/pipeline_freqanalysis_contrast.m
@@ -3,18 +3,39 @@ if ~ft_hastoolbox('qsub',1)
     addpath /home/kriarm/git/fieldtrip/qsub;
 end
 
-subjects = strsplit(sprintf('s%.2d ', 1:10));
+subjects = strsplit(sprintf('s%.2d ', 1:28));
 subjects = subjects(~cellfun(@isempty, subjects));
 
 s6 = strcmp(subjects, 's06');
 subjects(s6) = []; % s06 dataset does not exist, empty it to prevent errors
+s9 = strcmp(subjects, 's09');
+subjects(s9) = [];
 
 num_sub = numel(subjects);
 display(subjects);
-ivars = {'log10perp', 'entropy'};
-runpipeline = 'regress';
+ivars = {'log10wf'};
+runpipeline = 'tertile';
 
 switch runpipeline
+    
+   case 'tertile'
+   sprintf('Doing the %s contrast...\n\n', runpipeline)
+    
+    for i = 1:numel(ivars)
+
+        ivarexp = ivars{i};
+
+        for k = 1:numel(subjects)
+
+            subject = subjects{k};
+            filename = 'hanning';
+            qsubfeval('pipeline_freqanalysis_contrast_tertile_qsub', subject, filename, ivarexp, ...
+                                                                'memreq', 1024^3 * 5,...
+                                                                'timreq', 30*60,...
+                                                                'batchid', 'streams_freq');
+        end
+
+    end
     
     case 'regress'
     sprintf('Doing the %s contrast...\n\n', runpipeline)
@@ -26,7 +47,7 @@ switch runpipeline
         for k = 1:numel(subjects)
 
             subject = subjects{k};
-            filename = 'dpss8';
+            filename = 'hanning';
             qsubfeval('pipeline_freqanalysis_contrast_qsub', subject, filename, ivarexp, ...
                                                                 'memreq', 1024^3 * 8,...
                                                                 'timreq', 30*60,...
@@ -34,7 +55,24 @@ switch runpipeline
         end
 
     end
-    
+   
+    case 'lexfreq'
+    sprintf('Doing the %s contrast...\n\n', runpipeline)
+
+    ivarexp = 'log10wf';
+
+    for k = 1:numel(subjects)
+
+            subject = subjects{k};
+            filename = 'hanning';
+            qsubfeval('pipeline_freqanalysis_contrast_lexfreq_qsub', subject, filename, ivarexp, ...
+                                                                'memreq', 1024^3 * 4,...
+                                                                'timreq', 30*60,...
+                                                                'batchid', 'streams_freq');
+
+    end
+
+
     
     case 'innerqr'
     sprintf('Doing the %s contrast...\n\n', runpipeline)
