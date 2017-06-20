91fcc7a8777d643c92fb0704883fbc3036ea3bd4
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-06-20 17:08:46 +0200

    added code for regressing word frequency stuff

--- matlab/streams_dics.m
+++ matlab/streams_dics.m
@@ -14,7 +14,7 @@ sourcemodelfile = fullfile(dir, 'preproc/anatomy', [subject '_sourcemodel.mat'])
 conditionsfile = fullfile(dir, 'analysis/freqanalysis/contrast/subject/tertile-split', [subject '_' ivar '_12-20.mat']); 
 
 % saving dir
-savedir = fullfile(dir, 'analysis', 'dics');
+savedir = fullfile(dir, 'analysis', 'dics', 'firstlevel');
 
 %% LOAD
 
@@ -45,12 +45,13 @@ cfg.trials = sel;
 data = ft_selectdata(cfg, data);
 % featuredata = ft_selectdata(cfg, featuredata);
 clear tmp;
-
+dics
 %% DO FREQANALYSIS
 
 cfg = [];
 cfg.method    = 'mtmfft';
 cfg.output    = 'powandcsd';
+cfg.keeptrials = 'yes';
 cfg.taper     = cfgfreq.taper;
 cfg.tapsmofrq = cfgfreq.tapsmofrq;
 cfg.foilim    = cfgfreq.foilim;
@@ -66,11 +67,11 @@ trl_indx_low = conditions.trial(:, low_column);
 trl_indx_high = conditions.trial(:, high_column);
 
 cfg = [];
-cfg.trial = trl_indx_low;
+cfg.trials = trl_indx_low;
 freq_low = ft_selectdata(cfg, freq);
 
 cfg = [];
-cfg.trial = trl_indx_high;
+cfg.trials = trl_indx_high;
 freq_high = ft_selectdata(cfg, freq);
 
 %% DICS
@@ -81,6 +82,7 @@ cfg.frequency           = cfgdics.freq;
 cfg.grid                = sourcemodel;
 cfg.grid.leadfield      = leadfield.leadfield;
 cfg.headmodel           = headmodel;
+cfg.keeptrials          = 'yes';
 cfg.dics.projectnoise   = 'yes';
 cfg.dics.lambda         = '5%';
 cfg.dics.keepfilter     = 'yes';
@@ -93,10 +95,39 @@ cfg.grid.filter = source_both.avg.filter;
 source_high  = ft_sourceanalysis(cfg, freq_high);
 source_low = ft_sourceanalysis(cfg, freq_low);
 
-%% SAVING
+%% REGRESS OUT WORD FREQUENCY DATA
 
-savename_high = fullfile(savedir, [subject '_sourceh']);
-savename_low = fullfile(savedir, [subject '_sourcel']);
+datadirivars = '/project/3011044.02/analysis/freqanalysis/ivars';
+fileivars = fullfile(datadirivars, [subject '_ivars2' '.mat']);
+load(fileivars);
+
+% eliminate nan trials (for ft_regressconfound())
+trialskeep = ~isnan(ivars.trial(:,2));
+
+cfg = [];
+cfg.trials = trialskeep;
+source_high = ft_selectdata(cfg, source_high);
+source_low  = ft_selectdata(cfg, source_low);
+
+trialinfo.trial = ivars.trial(trialskeep, :);
+trialinfo.label = ivars.label;
+
+if ~strcmp(ivar, 'log10wf') % if ivarexp is lex. fr. itself skip this step
+    
+    nuisance_vars = {'log10wf'}; % take lexical frequency as nuissance
+    confounds = ismember(trialinfo.label, nuisance_vars); % logical with 1 in the columns for nuisance vars
+
+    cfg  = [];
+    cfg.confound = trialinfo.trial(:, confounds);
+    cfg.beta = 'no';
+    source_high = ft_regressconfound(cfg, source_high);
+
+end
+
+%% SAVING source rec
+
+savename_high = fullfile(savedir, ivar, [subject '_sourceh']);
+savename_low = fullfile(savedir, ivar, [subject '_sourcel']);
 
 save(savename_low, 'source_low');
 save(savename_high, 'savename_high');
