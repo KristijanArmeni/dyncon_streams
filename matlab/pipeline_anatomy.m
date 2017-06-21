bd3a46ef105be7d7e0683109d18761fc3e8e023b
Kristijan Armeni <kriarm@mentat001.dccn.nl>
2017-06-21 13:41:29 +0200

    mostly cosmetics with subject strings etc.

--- matlab/pipeline_anatomy.m
+++ matlab/pipeline_anatomy.m
@@ -1,7 +1,7 @@
 %% MRI PREPROCESSING, HEADMODEL, SOURCEMODEL
 
 % PREPOCESSING
-subject = 's05';
+subject = 's15';
 
 % converting dicoms to mgz format
 streams_anatomy_dicom2mgz(subject);
@@ -41,25 +41,49 @@ if ~ft_hastoolbox('qsub',1)
     addpath /home/common/matlab/fieldtrip/qsub;
 end
 
-qsubfeval('qsub_streams_anatomy_freesurfer2', subject,...
-          'memreq', 1024^3 * 7,...
-          'timreq', 720*60,...
-          'batchid', 'streams_freesurfer2');
+subjects = strsplit(sprintf('s%.2d ', 12:27));
+subjects = subjects(~cellfun(@isempty, subjects));
 
+s6 = strcmp(subjects, 's06');
+subjects(s6) = []; % s06 dataset does not exist, empty it to prevent errors
+s9 = strcmp(subjects, 's09');
+subjects(s9) = []; % s06 dataset does not exist, empty it to prevent errors
+
+num_sub = numel(subjects);
+display(subjects);
+
+for k = 1:num_sub
+    
+    subject = subjects{k};
+    
+    qsubfeval('qsub_streams_anatomy_freesurfer2', subject,...
+              'memreq', 1024^3 * 7,...
+              'timreq', 720*60,...
+              'batchid', 'streams_freesurfer2');
+
+end
 %% Post-processing Freesurfer script: workbench HCP tool
 if ~ft_hastoolbox('qsub',1)
     addpath /home/common/matlab/fieldtrip/qsub;
 end
 
-subjects = {'s03' 's04' 's05' 's07' 's08' 's09' 's10'};
+subjects = strsplit(sprintf('s%.2d ', 12:27));
+subjects = subjects(~cellfun(@isempty, subjects));
 
-for k = 1:numel(subjects)
+s6 = strcmp(subjects, 's06');
+subjects(s6) = []; % s06 dataset does not exist, empty it to prevent errors
+s9 = strcmp(subjects, 's09');
+subjects(s9) = []; % s06 dataset does not exist, empty it to prevent errors
+
+num_sub = numel(subjects);
+display(subjects);
+
+for k = 1:num_sub
   
   subject = subjects{k};
   qsubfeval('streams_anatomy_workbench', subject,...
             'memreq', 1024^3 * 6,...
-            'timreq', 480*60,...
-            'batchid', 'streams_workbench');
+            'timreq', 480*60);
           
 end
 
@@ -69,11 +93,13 @@ streams_anatomy_coregistration_qc(subject);
 
 
 %%  Sourcemodel
-subjects = {'s04' 's05' 's07' 's08' 's09' 's10'};
+
 for h = 1:numel(subjects)
 
   subject = subjects{h};
-  streams_anatomy_sourcemodel2d(subject);
+  qsubfeval('streams_anatomy_sourcemodel2d', subject, ...
+            'memreq', 1024^3 * 5, ...
+            'timreq', 20*60);
 
        
 end
@@ -86,8 +112,7 @@ for i = 1:numel(subjects)
   subject = subjects{i};
   qsubfeval('streams_anatomy_headmodel', subject, ...
             'memreq', 1024^3 * 5,...
-            'timreq', 20*60,...
-            'batchid', 'streams_headmodel')
+            'timreq', 20*60);
 
 end
 
@@ -99,8 +124,7 @@ for h = 1:numel(subjects)
   subject = subjects{h};
   qsubfeval('streams_leadfield', subject, ...
             'memreq', 1024^3 * 6,...
-            'timreq', 25*60,...
-            'batchid', 'streams_headmodel');
+            'timreq', 25*60);
 
        
 end
