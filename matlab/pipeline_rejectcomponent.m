8484185937995cbd28c45f95e3d64b5c0b6989d4
Kristijan Armeni <kriarm@mentat002.dccn.nl>
2017-08-10 15:38:49 +0200

    added the use of streams_util_subjestring()

--- matlab/pipeline_rejectcomponent.m
+++ matlab/pipeline_rejectcomponent.m
@@ -5,16 +5,9 @@ if ~ft_hastoolbox('qsub',1)
     addpath /home/kriarm/git/fieldtrip/qsub;
 end
 
-subjects = strsplit(sprintf('s%.2d ', 10:28));
-subjects = subjects(~cellfun(@isempty, subjects));
+[subjects, num_sub] = streams_util_subjectstring(2:28, {'s06'});
 
-s6 = strcmp(subjects, 's06');
-subjects(s6) = []; % s06 dataset does not exist, empty it to prevent errors
-
-num_sub = numel(subjects);
-display(subjects);
-
-for i = 1:numel(subjects)
+for i = 1:num_sub
     
     subject = subjects{i};
     qsubfeval('streams_rejectcomponent', subject, ...)
