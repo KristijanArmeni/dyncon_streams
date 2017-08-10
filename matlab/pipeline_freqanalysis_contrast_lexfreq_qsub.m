750a78b730e940f9e62d18ae1f2ec14666c9c4eb
Kristijan Armeni <kriarm@mentat002.dccn.nl>
2017-08-10 15:40:49 +0200

    ../language/streams_definecontrast.m
    now reads in the newly recomputed featuredata

--- matlab/pipeline_freqanalysis_contrast_lexfreq_qsub.m
+++ matlab/pipeline_freqanalysis_contrast_lexfreq_qsub.m
@@ -1,21 +1,15 @@
-function pipeline_freqanalysis_contrast_tertile_qsub(subject, inputargs)
-
-%% intialize
-
-ivarexp = ft_getopt(inputargs, 'ivarexp'); % the first input must not be called 'varargin', else matlab complains
-dohigh = ft_getopt(inputargs, 'dohigh', 0);
-filename = ft_getopt(inputargs, 'filename');
+function pipeline_freqanalysis_contrast_lexfreq_qsub(subject, filename, ivarexp)
 
 datadir = '/project/3011044.02/analysis/freqanalysis';
 datadirivars = '/project/3011044.02/analysis/freqanalysis/ivars';
-savedir = '/project/3011044.02/analysis/freqanalysis/contrast/subject/tertile-split';
+savedir = '/project/3011044.02/analysis/freqanalysis/contrast/subject/lexfreq';
 
 filefreq = fullfile(datadir, [subject '_' filename '.mat']); %.mat files
 fileivars = fullfile(datadirivars, [subject '_ivars2' '.mat']);
-
 load(filefreq) % loads in the freq variable
 load(fileivars) % loads in the ivars variable
 
+%% intialize
 taper = freq.cfg.previous.taper;
 isdpss = strcmp(taper, 'dpss');
 
@@ -23,9 +17,7 @@ isdpss = strcmp(taper, 'dpss');
 if isdpss; tapsmofrq = freq.cfg.previous.tapsmofrq; end
 
 % determine the foi based on which taper and smooth was used on the data
-if     isdpss && tapsmofrq == 8 && dohigh; foi = [60 90];
-elseif isdpss && tapsmofrq == 8; foi = [30 60];
-elseif isdpss && tapsmofrq == 4 && dohigh; foi = [20 30];
+if     isdpss && tapsmofrq == 8; foi = [30 90];
 elseif isdpss && tapsmofrq == 4; foi = [12 20];
 else;                            foi = [4 8];
 end
@@ -41,37 +33,28 @@ freq = ft_selectdata(cfg, freq);
 trialinfo.trial = ivars.trial(trialskeep, :);
 trialinfo.label = ivars.label;
 
-%% regress out lexical frequency
+%% regress out number of characters and lexical frequency
+% 
+% nuisance_vars = {'nchar', 'log10wf'};
+% confounds = ismember(trialinfo.label, nuisance_vars);
+% 
+% cfg  = [];
+% cfg.confound = trialinfo.trial(:, confounds);
+% cfg.beta = 'no';
+% freq = ft_regressconfound(cfg, freq);
 
-if ~strcmp(ivarexp, 'log10wf') % if ivarexp is lex. fr. itself skip this step
-    
-    nuisance_vars = {'log10wf'}; % take lexical frequency as nuissance
-    confounds = ismember(trialinfo.label, nuisance_vars); % logical with 1 in the columns for nuisance vars
-
-    cfg  = [];
-    cfg.confound = trialinfo.trial(:, confounds);
-    cfg.beta = 'no';
-    freq = ft_regressconfound(cfg, freq);
-
-end
-
-%% Split the data into high and low conditions
+%% Split the data into high and low conditions and control for frequency
 
 % find channel index
 col_exp = strcmp(trialinfo.label(:), ivarexp);
-ivar_exp = trialinfo.trial(:, col_exp); % pick the appropriate language variable (mean complexity for each trial)
+ivar_exp = trialinfo.trial(:, col_exp); % pick the appropriate language variable
 
-q = quantile(ivar_exp, [0.33 0.66]); % extract the two quantile values
-low_tertile = q(1);
-high_tertile = q(2);
+q = quantile(ivar_exp, [0.25 0.50 0.75]); % extract the three quantile values
+med = q(2); % median quartile
 
-% split into high and low tertile groups
-trl_indx_low = ivar_exp < low_tertile; % this gives a logical vector
-trl_indx_high = ivar_exp > high_tertile; 
-
-% create condition structure
-conditions.trial = [trl_indx_low, trl_indx_high];
-conditions.label = {'low', 'high'};
+% median split
+trl_indx_high = ivar_exp > med; % this gives a logical vector
+trl_indx_low = ivar_exp < med;
 
 % select data
 cfg = [];
@@ -101,7 +84,8 @@ foistr = [num2str(foi(1)) '-' num2str(foi(2))];
 filenameout = ['_' ivarexp '_' foistr];
 
 % save the info on preprocessing options used
-pipelinefilename = fullfile(savedir, ['s01' filenameout]);
+datecreated = char(datetime('today', 'Format', 'dd_MM_yy'));
+pipelinefilename = fullfile(savedir, ['s11' filenameout '_' datecreated]);
 
 if ~exist([pipelinefilename '.html'], 'file')
     cfgt = [];
@@ -111,9 +95,9 @@ if ~exist([pipelinefilename '.html'], 'file')
 end
 
 % save stat
-savename_stat = [subject filenameout];
-savename_stat = fullfile(savedir, savename_stat);
-save(savename_stat, 'stat', 'conditions'); % save trial indexes too
+savename_ttest = [subject filenameout];
+savename_ttest = fullfile(savedir, savename_ttest);
+save(savename_ttest, 'stat')
 
 
 end
