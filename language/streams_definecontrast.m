b87a02e9603f2f251a3cf0698df61887181c47de
Kristijan Armeni <kriarm@mentat002.dccn.nl>
2017-08-16 17:21:58 +0200

    added outputs, added epoching of featuredata, only contrast is saved if specified

--- language/streams_definecontrast.m
+++ language/streams_definecontrast.m
@@ -1,4 +1,4 @@
-function streams_definecontrast(subject)
+function [data, featuredata, contrast] = streams_epochdefinecontrast(subject, varargin)
 
 % streams_definecontrast(subject) averages language data from featuredata
 % obtained from streams_preprocessing_language() and computes tertile split
@@ -8,34 +8,36 @@ function streams_definecontrast(subject)
 
 %% INITIALIZE 
 
-datadir  = '/project/3011044.02/preproc/language';
+save     = ft_getopt(varargin, 'save', 0);
+
+datadir  = '/project/3011044.02/preproc/meg';
 savedir  = '/project/3011044.02/analysis/lng-contrast';
-megfile = fullfile('/project/3011044.02/preproc/meg', [subject, '_meg-clean']); % load in preprocessed meg data
+megfile  = fullfile('/project/3011044.02/preproc/meg', [subject, '_meg-clean']); % load in preprocessed meg data
+languagepreproc = fullfile(datadir, [subject, '_featuredata.mat']); % recomputed data (after the critical bugfix)
+
 savename = fullfile(savedir, subject);  % for the contrast structure
-savename2 = fullfile(datadir, subject); % save the new featuredata
 
-% load in the recomputed data (after the critical bugfix)
-languagepreproc = fullfile(datadir, subject);
-
-load(languagepreproc) % loads in the featuredata variable
-
-%% EPOCH FEATURE
-
-cfg = [];
-cfg.length = 1; % make a single trial 300 samples long
-featuredata = ft_redefinetrial(cfg, featuredata);
-
-%% ADHOC TRIAL REMOVAL (TO MATCH THE MEG DATA)
+%% LOADING
 
 data = []; % to prevent dynamic error assignment (?)
 load(megfile);
+load(languagepreproc) % loads in the featuredata variable
+
+%% EPOCH FEATURE and MEG DATA
+
+cfg = [];
+cfg.length  = 1; % make a single trial 300 samples long
+featuredata = ft_redefinetrial(cfg, featuredata);
+data        = ft_redefinetrial(cfg, data);
+
+%% ADHOC TRIAL REMOVAL
 
 sel = streams_cleanadhoc(data); % select trials with high variance (as was done for freqanalysis
 
 cfg = [];
-cfg.trials  = sel; % make sure featuredata has the same trials as MEG data
-featuredata = ft_selectdata(cfg, featuredata);
-data        = ft_selectdata(cfg, data);
+cfg.trials   = sel; % make sure featuredata has the same trials as MEG data
+featuredata  = ft_selectdata(cfg, featuredata);
+data         = ft_selectdata(cfg, data);
 
 %% AVERAGE FEATURE
 
@@ -45,61 +47,56 @@ selected_features = {'perplexity', 'entropy', 'log10wf'};
 % .trialinfolabel
 featuredata = streams_averagefeature(featuredata, selected_features);
 
-%% THROW OUT THE NANS HERE
-
-% log10wf    = strcmp(featureavg.label, 'log10wf');
-% trialskeep = ~isnan(featureavg.trial(:, log10wf));
-% 
-% featureavg.trialinfo = featureavg.trial(trialskeep, :); % select non-nan trials in all columns
-
 %% DO THE TERTILE SPLIT
 
-numvars = numel(selected_features);
+% load or compute the contrast
+if exist('savename', 'file')
+    load(savename)
+else
+    numvars = numel(selected_features);
 
-for i = 1:numvars
+    for i = 1:numvars
 
-    ivarexp = selected_features{i};
-    
-    % find channel index
-    col_exp = strcmp(featuredata.trialinfolabel(:), ivarexp);
-    ivar_exp = featuredata.trialinfo(:, col_exp); % pick the appropriate language variable (mean complexity for each trial)
+        ivarexp = selected_features{i};
 
-    q = quantile(ivar_exp, [0.33 0.66]); % extract the two quantile values
-    low_tertile = q(1);
-    high_tertile = q(2);
+        % find channel index
+        col_exp = strcmp(featuredata.trialinfolabel(:), ivarexp);
+        ivar_exp = featuredata.trialinfo(:, col_exp); % pick the appropriate language variable (mean complexity for each trial)
 
-    % split into high and low tertile groups
-    trl_indx_low = ivar_exp < low_tertile; % this gives a logical vector
-    trl_indx_high = ivar_exp > high_tertile; 
+        q = quantile(ivar_exp, [0.33 0.66]); % extract the two quantile values
+        low_tertile = q(1);
+        high_tertile = q(2);
 
-    % create the contrast structure
-    contrast(i).indepvar    = ivarexp;
-    contrast(i).label       = {'low', 'high'}; 
-    contrast(i).trial       = [trl_indx_low, trl_indx_high];
-    
+        % split into high and low tertile groups
+        trl_indx_low = ivar_exp < low_tertile; % this gives a logical vector
+        trl_indx_high = ivar_exp > high_tertile; 
+
+        % create the contrast structure
+        contrast(i).indepvar    = ivarexp;
+        contrast(i).label       = {'low', 'high'}; 
+        contrast(i).trial       = [trl_indx_low, trl_indx_high];
+
+    end
+
 end
 
 %% SAVING
 
-savenamedate      = fullfile(savedir, 's02');
-savenamedate2     = fullfile(datadir, 's02'); % for preproc/language
-datecreated       = char(datetime('today', 'Format', 'dd-MM-yy'));
-savenamedatefull  = [savenamedate '_' datecreated];
-savenamedatefull2 = [savenamedate2 '_' datecreated];
-dummy             = 'this is just a time stamp for streams_definecontrast()';
+if save
 
-fid = fopen([savenamedatefull '.txt'], 'wt');
-fprintf(fid, dummy);
-fclose(fid);
+    savenamedate      = fullfile(savedir, 's02');
+    datecreated       = char(datetime('today', 'Format', 'dd-MM-yy'));
+    savenamedatefull  = [savenamedate '_' datecreated];
+    dummy             = 'this is just a time stamp for streams_definecontrast()';
 
-fid = fopen([savenamedatefull2 '.txt'], 'wt');
-fprintf(fid, dummy);
-fclose(fid);
+    fid = fopen([savenamedatefull '.txt'], 'wt');
+    fprintf(fid, dummy);
+    fclose(fid);
 
-% save contrast, featuredata with new trialinfo and meg data
-save(savename, 'contrast')
-save(savename2, 'featuredata');
-save(megfile, 'data');
+    % save contrast, featuredata with new trialinfo and meg data
+    save(savename, 'contrast')
+
+end
 
 %% SUBFUNCTIONS
 
