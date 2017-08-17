9cd9028f3fc8d99339e415a1eec088cf34a36cdf
Kristijan Armeni <kriarm@mentat002.dccn.nl>
2017-08-17 11:29:17 +0200

    cleaning up and cosmetics, some comments and alignments

--- matlab/streams_freqanalysis.m
+++ matlab/streams_freqanalysis.m
@@ -1,15 +1,11 @@
-function [freq, data] = streams_freqanalysis(data, taper, tapsmooth)
+function [freq, data] = streams_freqanalysis(subject, taper, tapsmooth)
 %streams_freqanalysis() chunks the data into 1s long epochs and computes
 %powerspectra via ft_freqanalysis
 
 
-%% ADDITIONAL CLEANING STEP
-% remove the trials that, across the channel array, have high variance in the individual epochs
-sel = streams_cleanadhoc(data);
+%% EPOCHING & ADDITIONAL CLEANING STEP
 
-cfg = [];
-cfg.trials = sel;
-data = ft_selectdata(cfg, data);
+[data, ~, ~] = streams_epochdefinecontrast(subject);
 
 %% Meg planar
  
@@ -23,16 +19,19 @@ cfg.channel      = {'MEG'};
 cfg.trials       = 'all';
 cfg.neighbours   = ft_prepare_neighbours(cfg, data);
 
-data      = ft_megplanar(cfg, data);
+data             = ft_megplanar(cfg, data);
 
 %% do freqanalysis and combine planar if specified
 
-cfg = [];
-cfg.method = 'mtmfft';
-cfg.output = 'pow';
-cfg.taper = taper;
-if strcmp(taper, 'dpss'); cfg.tapsmofrq = tapsmooth; end
-cfg.keeptrials = 'yes';
+cfg               = [];
+cfg.method        = 'mtmfft';
+cfg.output        = 'pow';
+cfg.taper         = taper;
+if strcmp(taper, 'dpss')
+    cfg.tapsmofrq = tapsmooth;
+end
+cfg.keeptrials    = 'yes';
+
 freq = ft_freqanalysis(cfg, data);
 
 cfg = [];
--- matlab/streams_freqanalysis_contrast.m
+++ matlab/streams_freqanalysis_contrast.m
@@ -1,20 +1,18 @@
 function streams_freqanalysis_contrast(subject, inputargs)
 
-%% intialize
+%% INITIALIZE
 
 ivarexp         = ft_getopt(inputargs, 'ivarexp'); % the first input must not be called 'varargin', else matlab complains
 dohigh          = ft_getopt(inputargs, 'dohigh', 0);
 filename        = ft_getopt(inputargs, 'filename');
 
 datadir         = '/project/3011044.02/analysis/freqanalysis';
-conditionsfile  = fullfile('/project/3011044.02/analysis/lng-contrast/', [subject '.mat']);
 savedir         = '/project/3011044.02/analysis/freqanalysis/contrast/subject/';
+filefreq        = fullfile(datadir, [subject '_' filename '.mat']); % MEG power spectra .mat files
 
-filefreq        = fullfile(datadir, [subject '_' filename '.mat']); %MEG power spectra .mat files
+load(filefreq)       % loads in the freq variable 
 
-load(filefreq) % loads in the freq variable
-load(conditionsfile) % loads in the contrast structure
-
+% check for tapers
 taper = freq.cfg.previous.taper;
 isdpss = strcmp(taper, 'dpss');
 
@@ -22,89 +20,104 @@ isdpss = strcmp(taper, 'dpss');
 if isdpss; tapsmofrq = freq.cfg.previous.tapsmofrq; end
 
 % determine the foi based on which taper and smooth was used on the data
-if     isdpss && tapsmofrq == 8 && dohigh; foi = [60 90];
-elseif isdpss && tapsmofrq == 8;           foi = [30 60];
-elseif isdpss && tapsmofrq == 4 && dohigh; foi = [20 30];
-elseif isdpss && tapsmofrq == 4;           foi = [12 20];
-else;                                      foi = [4 8];
+if      isdpss && tapsmofrq == 8 && dohigh;  foi = [60 90];
+elseif  isdpss && tapsmofrq == 8;            foi = [30 60];
+elseif  isdpss && tapsmofrq == 4 && dohigh;  foi = [20 30];
+elseif  isdpss && tapsmofrq == 4;            foi = [12 20];
+else;                                        foi = [4 8];
 end
+
+%% LOAD IN EPOCHED FEATUREDATA WITH TRIALINFO AND CONTRAST STRUCTURE
+
+opt = {'save', 0};
+[~, featuredata, contrast] = streams_epochdefinecontrast(subject, opt);
 
 %% throw out nan trials based on log10wf column
 
-% select trials which have a value assigned in featureavg (needed for
-% regressconfound)
+selected_column        = strcmp(featuredata.trialinfolabel, 'log10wf');
+trialskeep             = logical(~isnan(featuredata.trialinfo(:, selected_column))); % keep only non-nan trials
 
-cfg = [];
-cfg.trials = trialskeep; % 'trialskeep' computed in streams_definecontrast()
-freq = ft_selectdata(cfg, freq);
+trialinfolabel         = featuredata.trialinfolabel; % store this because ft_selectdata below discards it
 
+cfg          = [];
+cfg.trials   = trialskeep;
+
+freq         = ft_selectdata(cfg, freq);
+featuredata  = ft_selectdata(cfg, featuredata);
+
+featuredata.trialinfolabel    = trialinfolabel; % plug trialinfolabel back in
 
 %% regress out lexical frequency
 
 if ~strcmp(ivarexp, 'log10wf') % if ivarexp is lex. fr. itself skip this step
     
     nuisance_vars = {'log10wf'}; % take lexical frequency as nuissance
-    confounds     = ismember(featureavg.label, nuisance_vars); % logical with 1 in the columns for nuisance vars
+    confounds     = ismember(featuredata.trialinfolabel, nuisance_vars); % logical with 1 in the columns for nuisance vars
 
     cfg  = [];
-    cfg.confound  = featureavg.trial(:, confounds);
+    cfg.confound  = featuredata.trialinfo(:, confounds);
     cfg.beta      = 'no';
+    
     freq          = ft_regressconfound(cfg, freq);
 
 end
 
 %% Split the data into high and low conditions
 
-% use the 'contrast' struct, precomputed in streams_definecontrast()
-ivarsel = strcmp({contrast.ivar}, ivarexp); % use the correct struct dimeension
-contrastsel = contrast(ivarsel); % chose a subset of the struct array
+% use the 'contrast' struct, computed in streams_epochdefinecontrast()
+ivarsel        = strcmp({contrast.indepvar}, ivarexp); % use the correct struct dimeension
+contrastsel    = contrast(ivarsel);                    % chose a subset of the struct array
 
-low_column = strcmp(contrastsel.label, 'low');
-high_column = strcmp(contrastsel.label, 'high');
+low_column     = strcmp(contrastsel.label, 'low');
+high_column    = strcmp(contrastsel.label, 'high');
 
-trl_indx_low = contrastsel.trial(:, low_column);
-trl_indx_high = contrastsel.trial(:, high_column);
+trl_indx_low   = contrastsel.trial(trialskeep, low_column);  % select non-NaN high complexity trials
+trl_indx_high  = contrastsel.trial(trialskeep, high_column); % select non-NaN low complexity trials
 
 % select data
 cfg = [];
-cfg.trials = trl_indx_low';
-freq_low = ft_selectdata(cfg, freq);
+cfg.trials     = trl_indx_low';
+freq_low       = ft_selectdata(cfg, freq);
 
 cfg = [];
-cfg.trials = trl_indx_high;
-freq_high = ft_selectdata(cfg, freq);
+cfg.trials     = trl_indx_high;
+freq_high      = ft_selectdata(cfg, freq);
 
 %% INDEPENDENT T-TEST
 
 design = [ones(1,size(freq_high.trialinfo,1)) ones(1,size(freq_low.trialinfo,1))*2];
 
 % independent between trials t-test
-cfg = [];
-cfg.method = 'montecarlo';
-cfg.statistic = 'indepsamplesT'; % for each subject do between trials (independent) t-test
-cfg.numrandomization = 0;
-cfg.frequency = foi;
-cfg.design = design;
+cfg                   = [];
+cfg.method            = 'montecarlo';
+cfg.statistic         = 'indepsamplesT'; % for each subject do between trials (independent) t-test
+cfg.numrandomization  = 0;
+cfg.frequency         = foi;             % determined based on the taper used in freqanalysis
+cfg.design            = design;
+
 stat = ft_freqstatistics(cfg, freq_high, freq_low);
 
 %% SAVE
 
-foistr = [num2str(foi(1)) '-' num2str(foi(2))];
-filenameout = ['_' ivarexp '_' foistr];
+foistr       = [num2str(foi(1)) '-' num2str(foi(2))];
+filenameout  = ['_' ivarexp '_' foistr];
 
 % save the info on preprocessing options used
-pipelinefilename = fullfile(savedir, ['s01' filenameout]);
+pipelinefilename = fullfile(savedir, ['s02' filenameout]);
 
 if ~exist([pipelinefilename '.html'], 'file')
-    cfgt = [];
-    cfgt.filename = pipelinefilename;
-    cfgt.filetype = 'html';
+    
+    cfgt           = [];
+    cfgt.filename  = pipelinefilename;
+    cfgt.filetype  = 'html';
     ft_analysispipeline(cfgt, stat);
+    
 end
 
 % save stat
 savename_stat = [subject filenameout];
 savename_stat = fullfile(savedir, savename_stat);
+
 save(savename_stat, 'stat'); % save trial indexes too
 
 
