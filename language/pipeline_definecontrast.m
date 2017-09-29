0365cc3dd5778dd858223dc72f0415f1df85ac04
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2017-09-29 09:02:16 +0200

    added opt input argument

--- language/pipeline_definecontrast.m
+++ language/pipeline_definecontrast.m
@@ -1,18 +1,14 @@
-clear all
 
-if ~ft_hastoolbox('qsub',1)
-    addpath /home/kriarm/git/fieldtrip/qsub;
-end
-
-%% INITIALIZE
-
-[subjects, num_sub] = streams_util_subjectstring(2:28, {'s01', 's06'});
+[subjects, num_sub] = streams_util_subjectstring(2:28, {'s06'});
 
 for i = 1:num_sub
     
     subject = subjects{i};
-    qsubfeval('streams_definecontrast', subject, ...
-                  'memreq', 1024^3 * 4,...
+    
+    opt = {'save', 1};
+    
+    qsubfeval('streams_epochdefinecontrast', subject, opt, ...
+                  'memreq', 1024^3 * 12,...
                   'timreq', 30*60, ...
                   'matlabcmd', 'matlab2016b');
     
