61bb13e19c91e986004961b0d4129d9d2390f5df
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2017-09-29 08:49:40 +0200

    it now saves lightwight stat structures without past cfg for faster loading

--- matlab/streams_freqanalysis_groupcontrast.m
+++ matlab/streams_freqanalysis_groupcontrast.m
@@ -11,8 +11,9 @@ prefix = [subjects{1} '-' subjects{end}];
 filename_stat = [ivar '_' foi '.mat'];
 
 % create strings for saving
-savename_stat_all = fullfile(datadir, [prefix '_' filename_stat]);
+savename_stat_all   = fullfile(datadir, [prefix '_' filename_stat]);
 savename_stat_group = fullfile(savedir, [prefix '_' ivar '_' foi '.mat']);
+savename_stat4plot   = fullfile(savedir, [prefix '_' ivar '_' foi '_4plot.mat']);
 
 stat_all = cell(num_sub, 1);
 
@@ -27,7 +28,7 @@ if ~exist(savename_stat_all, 'file')
 
         subject = subjects{k};
 
-        file_T = fullfile(datadir, [subject '_' filename_stat]);
+        file_T  = fullfile(datadir, [subject '_' filename_stat]);
         load(file_T)
         
         stat_all{k} = stat;
@@ -42,8 +43,10 @@ if ~exist(savename_stat_all, 'file')
     fprintf('Saving %s... \n', savename_stat_all)
 
 else % just load it
+    
     fprintf('Loading %s: \n', savename_stat_all);
     load(savename_stat_all)
+
 end
 
 %% Freq statistics
@@ -51,18 +54,20 @@ fprintf('Doing second level stats on: \n\n')
 
 % import preproc data for grad information in neighbourhoud chan definition
 load('/project/3011044.02/preproc/meg/s02_meg-clean.mat');
-neighdata = stat_all{1};
+neighdata      = stat_all{1};
 neighdata.grad = data.grad;
 
 % Create the null structure
 data_N = stat_all;
-for k = 1:numel(data_N); data_N{k}.stat(:,:) = 0; end
+for k = 1:numel(data_N)
+    data_N{k}.stat(:,:) = 0;
+end
 
 % specify design matrix
-design = zeros(2, 2*num_sub);
-design(1, 1:num_sub) = 1:num_sub;
+design                           = zeros(2, 2*num_sub);
+design(1, 1:num_sub)             = 1:num_sub;
 design(1, num_sub + 1:num_sub*2) = 1:num_sub;
-design(2, 1:num_sub) = 1;
+design(2, 1:num_sub)             = 1;
 design(2, num_sub + 1:num_sub*2) = 2;
 
 % second-level t-test
@@ -77,25 +82,23 @@ cfg.neighbours       = ft_prepare_neighbours(cfg_neighb, neighdata);
 cfg.method           = 'montecarlo';
 cfg.parameter        = 'stat';
 cfg.statistic        = 'depsamplesT';
-% cfg.correctm         = 'cluster';
-% cfg.tail             = 0; % two-sided test
-% cfg.clustertail      = 0;
-% cfg.alpha            = 0.025; % adjust alpha-level for two-sided test
-% cfg.correcttail = 'prob';  
-% cfg.clusteralpha     = 0.025; % adjust cluster alpha-level for two-sided test 
-cfg.numrandomization = 0;
-cfg.design = design;
-cfg.uvar = 1;
-cfg.ivar = 2;
+cfg.correctm         = 'cluster';
+cfg.alpha            = 0.025; % adjust alpha-level for two-sided test
+cfg.correcttail      = 'prob';  
+cfg.numrandomization = 1000;
+cfg.design           = design;
+cfg.uvar             = 1;
+cfg.ivar             = 2;
 
 % optional:
-cfg.avgoverfreq = 'no';
-%cfg.frequency = [40 60];
+cfg.avgoverfreq      = 'yes';
 
-stat_group = ft_freqstatistics(cfg, stat_all{:}, data_N{:});
+stat_group           = ft_freqstatistics(cfg, stat_all{:}, data_N{:});
+stat4plot            = rmfield(stat_group, 'cfg');
 
 %% Saving
 
 fprintf('Saving %s... \n', savename_stat_group)
 
 save(savename_stat_group, 'stat_group');
+save(savename_stat4plot, 'stat4plot');
