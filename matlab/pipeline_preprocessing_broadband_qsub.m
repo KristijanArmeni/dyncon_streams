4af7106e00d61bf4bd6bf4dfe8b752f01e1fffa8
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2017-09-29 09:00:24 +0200

    syntactic adjustments in call to streams_preprocessing

--- matlab/pipeline_preprocessing_broadband_qsub.m
+++ matlab/pipeline_preprocessing_broadband_qsub.m
@@ -6,51 +6,48 @@ savedir = '/project/3011044.02/preproc/meg';
 
 % preprocessing options
 fsample = ft_getopt(optarg, 'fsample');
+feature = {'nchar' 'duration' 'log10wf'  'perplexity' 'entropy'};
 
 %% Preprocessing, band-pass filtering, and downsampling
                         
-[data, ~, audio] = streams_preprocessing(subject, ...
-                            'audiofile', audiofile, ...
-                            'lpfreq', [], ...
-                            'hpfreq', [], ...
-                            'dftfreq', [49 51; 99 101; 149 151], ...
-                            'docomp', 1, ...
-                            'dospeechenvelope', 1, ...
-                            'fsample', fsample);
-
-                        
+[~, ~, ~, featuredata] = streams_preprocessing(subject, ...
+                                'audiofile', audiofile, ...
+                                'feature', feature, ...
+                                'lpfreq', [], ...
+                                'hpfreq', [], ...
+                                'dftfreq', [49 51; 99 101; 149 151], ...
+                                'dospeechenvelope', 1, ...
+                                'fsample', fsample);
+   
 %% Saving
 
-% construct naming variable
-% lowpassfreq = sprintf('%02d', lpfreq);
-% highpassfreq = sprintf('%02d', hpfreq);
-% frequency_band = [highpassfreq, '-', lowpassfreq];
-
-%pipelinename = ['_' audiofile '_' frequency_band '_' num2str(fsample)];
-
-savenamemeg = [subject.name '_meg'];
-savenamemeg = fullfile(savedir, savenamemeg);
+%savenamemeg = [subject.name '_meg'];
+%savenamemeg = fullfile(savedir, savenamemeg);
 
 % savenameeeg = [subject.name '_eeg'];
 % savenameeeg = fullfile(savedir, savenameeeg);
 
-savenameaudio = [subject.name '_aud'];
-savenameaudio = fullfile(savedir, savenameaudio);
+% savenameaudio = [subject.name '_aud'];
+% savenameaudio = fullfile(savedir, savenameaudio);
+
+savenamefeaturedata = [subject.name '_featuredata'];
+savenamefeaturedata = fullfile(savedir, savenamefeaturedata);
 
 datecreated = char(datetime('today', 'Format', 'dd-MM-yy'));
-pipelinefilename = fullfile(savedir, ['s01_meg_' datecreated]);
+pipelinefilename = fullfile(savedir, ['s02_meg_' datecreated]);
 
 % save the pipeline if not yet saved
-if ~exist([pipelinefilename '.html'], 'file')
+if ~exist([pipelinefilename '.html'], 'file') && exist('data', 'var')
     cfgt = [];
     cfgt.filename = pipelinefilename;
     cfgt.filetype = 'html';
     ft_analysispipeline(cfgt, data);
 end
     
-save(savenamemeg, 'data');
+% save(savenamemeg, 'data');
 % save(savenameeeg, 'eeg');
-save(savenameaudio, 'audio');
+% save(savenameaudio, 'audio');
+save(savenamefeaturedata, 'featuredata');
 
 
 end
