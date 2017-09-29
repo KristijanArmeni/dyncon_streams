df9d566f263e406157cc789712f3c8aae299bae1
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2017-09-29 08:51:42 +0200

    mostly changes to variable naming etc.

--- matlab/pipeline_freqanalysis_groupcontrast.m
+++ matlab/pipeline_freqanalysis_groupcontrast.m
@@ -1,9 +1,15 @@
 
-ivars = {'entropy', 'perplexity', 'log10wf'};
-freqs = {'4-8', '12-20', '20-30'};
+ivars = {'entropy', 'perplexity'};
+freqs = {'1-3', '4-8', '8-12', '12-20', '20-30', '30-60', '60-90'};
 
-datadir = '/project/3011044.02/analysis/freqanalysis/contrast/subject3/';
-savedir = '/project/3011044.02/analysis/freqanalysis/contrast/group3/';
+% /subject1 --> basic
+% /subject2 --> prunned
+% /subject3 --> epoching to 0.5
+% /subject4 --> mean over the number of words
+% /subject5 --> regressed for lexfr && audio_avg, altmean == 0, prune == 0
+
+datadir = '/project/3011044.02/analysis/freqanalysis/contrast/subject5/';
+savedir = '/project/3011044.02/analysis/freqanalysis/contrast/group5/';
 
 % variable and frequency loop
 for j = 1:numel(freqs)
@@ -16,7 +22,7 @@ for j = 1:numel(freqs)
         
         qsubfeval('streams_freqanalysis_groupcontrast', ivar, foi, datadir, savedir, ...
                                           'memreq', 1024^3 * 12,...
-                                          'timreq', 240*60);
+                                          'timreq', 90*60);
     end
 
 end
