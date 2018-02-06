99328fe3b53b0be6cd9fdaa61620c039637c39a1
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2018-02-06 14:58:57 +0100

    MAJ.sem; feature vector can now have row dimension > 1

--- matlab/get_time_series.m
+++ matlab/get_time_series.m
@@ -1,18 +1,17 @@
 function [ time , feature_value_vector, trl] = get_time_series( combined_data, feature, sampling_rate, select )
-
-% GET_STREAMS_TIME_SERIES creates a vector and time axis of a specified
+% get_time_series() creates a vector and time axis of a specified
 % feature from the computational model output, at a specified sampling
 % rate.
 %
 % Use as:
-%   [time, data] = get_time_series(combined_data, feature, sampling_rate)
+%   [time, data] = get_time_series(combined_data, feature, sampling_rate, select)
 %
 % Input arguments:
 %   combined_data = struct_array, the output of COMBINE_DONDERS_TEXTGRID
 %   feature       = string, specifying which feature to use
 %   sampling_rate = integer scalar, specifying the sampling rate
 %   select        = string, 'all', 'content', 'noonset' or 'content_noonset', 
-%                   whether or not to include content and/or onset words in creating the vector
+%                    whether or not to include content and/or onset words in creating the vector
 %
 % Output arguments:
 %   time = vector, specifying the time axis
@@ -26,9 +25,11 @@ function [ time , feature_value_vector, trl] = get_time_series( combined_data, f
 %            sampling_rate, and are relative to the beginning of the
 %            stimulus. note, also, that the samples have an offset of 1.
 
-end_time_point = max([combined_data.end_time]);     % the length of the 
-time = linspace(0, end_time_point, end_time_point * sampling_rate + 1); 
-feature_value_vector = zeros(1, numel(time))+nan; % initialize as NaN so that missing data takes this value
+end_time_point       = max([combined_data.end_time]);     % the length of the 
+time                 = linspace(0, end_time_point, end_time_point * sampling_rate + 1); 
+
+feature_dim          = size(combined_data(1).(feature), 1);  % determine dimensionality
+feature_value_vector = zeros(feature_dim, numel(time))+nan; % initialize as NaN so that missing data takes this value
 
 begtim = [combined_data.start_time]';
 endtim = [combined_data.end_time]';
@@ -51,36 +52,42 @@ for k = 1:numel(begtim)
   isonset           = ismember(combined_data(k).word_, [0, 1]); % logical, check if word index is 0 or 1 (indicating onset position)
   feature_value     = combined_data(k).(feature); % curent feature value to be assigned
   
+  % if feature is vector-based, create matrix to prevent assignment
+  % dimension mismath below
+  if isvector(feature_value)
+      feature_value = repmat(feature_value, [1, endsmp-begsmp + 1]);
+  end
+  
   % make sure word selection only operates on model metrics
-  if ismember(feature, {'entropy', 'perplexity', 'log10wf'})
+  if ismember(feature, {'entropy', 'perplexity', 'semdist', 'embedding', 'log10wf'})
       switch select
 
           case 'all' % assign value to all parsed words
 
-          feature_value_vector(begsmp:endsmp) = feature_value;
+          feature_value_vector(:, begsmp:endsmp) = feature_value;
 
           case 'content' % assign value to the vector only if it is content word
 
           if iscontent
-            feature_value_vector(begsmp:endsmp) = feature_value;
+            feature_value_vector(:, begsmp:endsmp) = feature_value;
           end
 
           case 'noonset' % select only words not indexed as 0 or 1
 
           if ~isonset
-            feature_value_vector(begsmp:endsmp) = feature_value;
+            feature_value_vector(:, begsmp:endsmp) = feature_value;
           end
 
           case 'content_noonset' % select content only, sentence-non initial words
 
           if iscontent && ~isonset
-            feature_value_vector(begsmp:endsmp) = feature_value;
+            feature_value_vector(:, begsmp:endsmp) = feature_value;
           end    
       end
   % if it is not complexity score or lex.freq quantify all words
   else     
       
-      feature_value_vector(begsmp:endsmp) = feature_value;
+      feature_value_vector(:, begsmp:endsmp) = feature_value;
   
   end    
 end
@@ -95,6 +102,6 @@ trl = zeros(numel(combined_data),4)+nan;
 trl(existtime,1) = round([combined_data(existtime).start_time]*sampling_rate);
 trl(existtime,2) = round([combined_data(existtime).end_time]*sampling_rate);
 trl(existtime,3) = 1:sum(existtime);
-trl(existtime,4) = [combined_data(existtime).(feature)];
+%trl(existtime,4) = [combined_data(existtime).(feature)]; FIXME, commenting out, temporary measure
 
 end
