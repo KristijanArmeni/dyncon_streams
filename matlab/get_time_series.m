4c6d3e90e7e000018582702108f8aa482e28ed38
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2018-02-21 12:03:28 +0100

    MAJ.sem.syn; added <shape> parameter which allows for flexible creation of either stick- or box-shaped predictors, also added <cfg> input argument for options, some book-keeping changes in variable names

--- matlab/get_time_series.m
+++ matlab/get_time_series.m
@@ -1,18 +1,23 @@
-function [ time , feature_value_vector, trl] = get_time_series( combined_data, feature, sampling_rate, select )
+function [ time , feature_value_vector, trl] = get_time_series(cfg, combined_data )
 % get_time_series() creates a vector and time axis of a specified
 % feature from the computational model output, at a specified sampling
 % rate.
 %
 % Use as:
-%   [time, data] = get_time_series(combined_data, feature, sampling_rate, select)
+%   [time, data] = get_time_series(cfg, combined_data)
 %
 % Input arguments:
 %   combined_data = struct_array, the output of COMBINE_DONDERS_TEXTGRID
+% 
+% Configuration (cfg) fields:
 %   feature       = string, specifying which feature to use
 %   sampling_rate = integer scalar, specifying the sampling rate
 %   select        = string, 'all', 'content', 'noonset' or 'content_noonset', 
 %                    whether or not to include content and/or onset words in creating the vector
-%
+%   shape         = string, 'box' or 'stick' specifying whether feature vector is a stick
+%                   function or a box-shaped function (i.e. with plateaus
+%                   for word duration)
+% 
 % Output arguments:
 %   time = vector, specifying the time axis
 %   data = vector, specifying the feature values as a 'block regressor'
@@ -25,8 +30,13 @@ function [ time , feature_value_vector, trl] = get_time_series( combined_data, f
 %            sampling_rate, and are relative to the beginning of the
 %            stimulus. note, also, that the samples have an offset of 1.
 
+feature = ft_getopt(cfg, 'feature');
+fsample = ft_getopt(cfg, 'fsample');
+select  = ft_getopt(cfg, 'select', 'all');
+shape   = ft_getopt(cfg, 'shape');
+
 end_time_point       = max([combined_data.end_time]);     % the length of the 
-time                 = linspace(0, end_time_point, end_time_point * sampling_rate + 1); 
+time                 = linspace(0, end_time_point, end_time_point * fsample + 1); 
 
 feature_dim          = size(combined_data(1).(feature), 1);  % determine dimensionality
 feature_value_vector = zeros(feature_dim, numel(time))+nan; % initialize as NaN so that missing data takes this value
@@ -43,19 +53,29 @@ endtim = endtim(sel);
 combined_data = combined_data(sel);
 
 %% loop over and create feture vector at MEG sampling rate based on model data
+
+iscontent         = [combined_data(:).iscontent];               % logical, check .iscontent field
+isonset           = ismember([combined_data(:).word_], [0, 1]); % logical vector, check if word index is 0 or 1 (indicating onset position)
+feature_values    = [combined_data(:).(feature)];               % vector of feature values to be assigned
+
 for k = 1:numel(begtim)
     
   begsmp = nearest(time, begtim(k));
   endsmp = nearest(time, endtim(k));
+  value  = feature_values(k);
   
-  iscontent         = combined_data(k).iscontent; % logical, check .iscontent field
-  isonset           = ismember(combined_data(k).word_, [0, 1]); % logical, check if word index is 0 or 1 (indicating onset position)
-  feature_value     = combined_data(k).(feature); % curent feature value to be assigned
+  % determine the shape of the function
+  switch shape
+    case 'stick'
+        plateau = begsmp;        % this will create stick function feature vectors (no plateau effectively)
+    case 'box'
+        plateau = begsmp:endsmp; % this creates a plateau
+  end
   
   % if feature is vector-based, create matrix to prevent assignment
   % dimension mismath below
-  if isvector(feature_value)
-      feature_value = repmat(feature_value, [1, endsmp-begsmp + 1]);
+  if ~isscalar(value)
+      value = repmat(value, [1, endsmp-begsmp + 1]);
   end
   
   % make sure word selection only operates on model metrics
@@ -64,30 +84,30 @@ for k = 1:numel(begtim)
 
           case 'all' % assign value to all parsed words
 
-          feature_value_vector(:, begsmp:endsmp) = feature_value;
+          feature_value_vector(:, plateau) = value;
 
           case 'content' % assign value to the vector only if it is content word
 
-          if iscontent
-            feature_value_vector(:, begsmp:endsmp) = feature_value;
+          if iscontent(k)
+            feature_value_vector(:, plateau) = value;
           end
 
           case 'noonset' % select only words not indexed as 0 or 1
 
-          if ~isonset
-            feature_value_vector(:, begsmp:endsmp) = feature_value;
+          if ~isonset(k)
+            feature_value_vector(:, plateau) = value;
           end
 
           case 'content_noonset' % select content only, sentence-non initial words
 
-          if iscontent && ~isonset
-            feature_value_vector(:, begsmp:endsmp) = feature_value;
+          if iscontent(k) && ~isonset(k)
+            feature_value_vector(:, plateau) = value;
           end    
       end
   % if it is not complexity score or lex.freq quantify all words
   else     
       
-      feature_value_vector(:, begsmp:endsmp) = feature_value;
+      feature_value_vector(:, plateau) = value;
   
   end    
 end
@@ -99,8 +119,8 @@ for i=1:numel(combined_data)
   existtime(i) = ~isempty(combined_data(i).start_time);
 end
 trl = zeros(numel(combined_data),4)+nan;
-trl(existtime,1) = round([combined_data(existtime).start_time]*sampling_rate);
-trl(existtime,2) = round([combined_data(existtime).end_time]*sampling_rate);
+trl(existtime,1) = round([combined_data(existtime).start_time]*fsample);
+trl(existtime,2) = round([combined_data(existtime).end_time]*fsample);
 trl(existtime,3) = 1:sum(existtime);
 %trl(existtime,4) = [combined_data(existtime).(feature)]; FIXME, commenting out, temporary measure
 
