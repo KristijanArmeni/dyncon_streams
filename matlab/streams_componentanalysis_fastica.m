18c1f8c297a00b96805efa1a0ffb1f7f898c9abc
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2018-07-18 09:33:11 +0200

    added streams_existfile() syntax

--- matlab/streams_componentanalysis_fastica.m
+++ matlab/streams_componentanalysis_fastica.m
@@ -2,22 +2,33 @@ function [comp]  = streams_componentanalysis_fastica(subject)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-datadir = '/project/3011044.02/preproc/meg';
-d = [subject '_meg2.mat'];
-datafile = fullfile(datadir, d);
-compfile = fullfile(datadir, [subject '_comp2.mat']);
 
-load(datafile);
 
-cfg                   = [];
-cfg.method            = 'fastica';
-cfg.fastica.lastEig   = 80;
-cfg.fastica.g         = 'tanh';
-cfg.channel           = 'MEG';
-cfg.numcomponent      = 20;
-comp                  = ft_componentanalysis(cfg, data);
+[status, filename] = streams_existfile([subject.name, '_compfica.mat']);
 
-save(compfile, 'comp');
+if status
+  load(filename);
+else
+    
+    datadir = '/project/3011044.02/preproc/meg';
+    d = [subject '_meg.mat'];
+    datafile = fullfile(datadir, d);
+    compfile = fullfile(datadir, [subject '_comp.mat']);
 
+    load(datafile);
+
+    cfg                   = [];
+    cfg.method            = 'fastica';
+    cfg.fastica.lastEig   = 80;
+    cfg.fastica.g         = 'tanh';
+    cfg.channel           = 'MEG';
+    cfg.numcomponent      = 20;
+    comp                  = ft_componentanalysis(cfg, data);
+    
+    fprintf()
+    save(compfile, 'comp');
+
+    
 end
 
+ 
