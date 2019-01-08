374cf4147356b915ff20a8fc96a4392f3129c1af
Kristijan Armeni <kriarm@mentat004.dccn.nl>
2019-01-08 10:35:44 +0100

    added code for computing coherence over different spatial locations

--- matlab/pipeline_coherence_lcmv.m
+++ matlab/pipeline_coherence_lcmv.m
@@ -4,14 +4,15 @@
 datadir   = '/project/3011044.02/preproc/';
 savedir   = '/project/3011044.02/analysis/coherence/source/subject';
 
-%% SUBJECT AND VARIABLE LOOP
+%% SUBJECT AND VARIABLE LOOP (ba42)
     
 for k = 1:numel(subjects)
 
     subject = subjects{k};
 
     inputargs = {'savedir', savedir, ...
-                 'datadir', datadir};
+                 'datadir', datadir, ...
+                 'roi', {'_42'}};
 
     qsubfeval('streams_coherence_lcmv', subject, inputargs, ...
                                                         'memreq', 1024^3 * 16,...
@@ -19,4 +20,37 @@ for k = 1:numel(subjects)
 end
         
 
+%% COHERENCE FOR UPPER AND LOWER BANKS 
+
+[subjects, num_sub] = streams_util_subjectstring([13:20, 22], {'s06', 's09'});
+
+for k = 1:numel(subjects)
+
+    subject = subjects{k};
+
+    inputargs = {'savedir', savedir, ...
+                 'datadir', datadir, ...
+                 'roi', {'_40', '_42', '_43'}};
+
+    qsubfeval('streams_coherence_lcmv', subject, inputargs, ...
+                                                        'memreq', 1024^3 * 30,...
+                                                        'timreq', 75*60);
+end
+
     
+%% COHERENCE FOR MAXIMAL DICS COHERENCE LOCATIONS
+
+[subjects, num_sub] = streams_util_subjectstring([2:28], {'s06', 's09'});
+
+for k = 1:numel(subjects)
+
+    subject = subjects{k};
+
+    inputargs = {'savedir', savedir, ...
+                 'datadir', datadir, ...
+                 'roi', {'dicsmax'}};
+
+    qsubfeval('streams_coherence_lcmv', subject, inputargs, ...
+                                                        'memreq', 1024^3 * 30,...
+                                                        'timreq', 75*60);
+end
