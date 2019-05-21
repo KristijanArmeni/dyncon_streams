f7b9cfea89c34b2b059d5ad0566785e917571af1
KristijanArmeni <kristijan.armeni@gmail.com>
2019-05-21 08:47:20 +0200

    included additional pipeline loops

--- matlab/pipeline_dics.m
+++ matlab/pipeline_dics.m
@@ -4,8 +4,8 @@
 ivars     = {'entropy'};
 freqbands = {'theta'};
 
-datadir = '/project/3011044.02/preproc/';
-savedir = '/project/3011044.02/analysis/freqanalysis/source/subject3';
+datadir = '/project/3011085.04/streams/preproc/';
+savedir = '/project/3011085.04/streams/analysis/freqanalysis/source/subject3';
 
 %% WITH EPOCH = 0.5 S
 
@@ -26,6 +26,7 @@ for k = 1:numel(freqbands)
                    'freqband', freqband, ...
                    'removeonset', 0, ...
                    'word_selection', 'all', ...
+                   'epochtype', 'onset-ignore', ...
                    'epochlength', 0.5, ...
                    'shift', [0 200 400 600], ...
                    'savewhat', 'source', ...
@@ -82,6 +83,198 @@ for k = 1:numel(freqbands)
         qsubfeval('streams_dics', subject, inpcfg, ...
                   'memreq', 1024^3*15,...
                   'timreq', 60*30, ...
+                  'matlabcmd', 'matlab2016b', ...
+                  'display', 'yes');
+        
+        end
+    
+        
+    end
+    
+    
+end
+
+%% With epochtype = 'onset-lock'
+
+[subjects, num_sub] = streams_util_subjectstring(2:10, {'s06', 's09'});
+
+ivars     = {'entropy', 'perplexity'};
+freqbands = {'gamma', 'high-gamma'};
+
+datadir = '/project/3011085.04/streams/preproc/';
+savedir = '/project/3011085.04/streams/analysis/freqanalysis/source/subject3/onset_lock';
+
+% Subject loop
+for k = 1:numel(freqbands)
+    
+    freqband = freqbands{k};
+    
+    for i = 1:numel(ivars)
+
+    indepvar = ivars{i};
+        
+        for kk = 1:num_sub
+
+        subject = subjects{kk};
+        
+        inpcfg = {'indepvar', indepvar, ...
+                   'freqband', freqband, ...
+                   'removeonset', 0, ...
+                   'word_selection', 'all', ...
+                   'epochtype', 'onset-lock', ...
+                   'epochlength', 0.5, ...
+                   'shift', [0 200 400 600], ...
+                   'savewhat', 'stat', ...
+                   'datadir', datadir, ...
+                   'savedir', savedir};
+        
+        qsubfeval('streams_dics', subject, inpcfg, ...
+                  'memreq', 1024^3*40,...
+                  'timreq', 60*40, ...
+                  'matlabcmd', 'matlab2016b', ...
+                  'display', 'yes');
+        
+        end
+    
+        
+    end
+    
+    
+end
+
+%% With epochtype = 'onset-lock-nooverlap'
+
+[subjects, num_sub] = streams_util_subjectstring(2:10, {'s06', 's09'});
+
+ivars     = {'entropy', 'perplexity'};
+freqbands = {'theta', 'alpha', 'beta', 'high-beta', 'gamma', 'high-gamma'};
+
+datadir = '/project/3011085.04/streams/preproc/';
+savedir = '/project/3011085.04/streams/analysis/freqanalysis/source/subject3/onset_lock_nooverlap';
+
+% Subject loop
+for k = 1:numel(freqbands)
+    
+    freqband = freqbands{k};
+    
+    for i = 1:numel(ivars)
+
+    indepvar = ivars{i};
+        
+        for kk = 1:num_sub
+
+        subject = subjects{kk};
+        
+        inpcfg = {'indepvar', indepvar, ...
+                   'freqband', freqband, ...
+                   'removeonset', 0, ...
+                   'word_selection', 'all', ...
+                   'epochtype', 'onset-lock-nooverlap', ...
+                   'epochlength', 0.5, ...
+                   'shift', [0 200 400 600], ...
+                   'savewhat', 'stat', ...
+                   'datadir', datadir, ...
+                   'savedir', savedir};
+        
+        qsubfeval('streams_dics', subject, inpcfg, ...
+                  'memreq', 1024^3*40,...
+                  'timreq', 60*40, ...
+                  'matlabcmd', 'matlab2016b', ...
+                  'display', 'yes');
+        
+        end
+    
+        
+    end
+    
+    
+end
+
+%% With epochtype = 'onset-lock-minoverlap'
+
+[subjects, num_sub] = streams_util_subjectstring(11:28, {'s06', 's09'});
+
+ivars     = {'entropy', 'perplexity'};
+freqbands = {'theta', 'alpha', 'beta', 'high-beta', 'gamma', 'high-gamma'};
+
+datadir = '/project/3011085.04/streams/preproc/';
+savedir = '/project/3011085.04/streams/analysis/freqanalysis/source/subject3/onset_lock_minoverlap';
+
+% Subject loop
+for k = 1:numel(freqbands)
+    
+    freqband = freqbands{k};
+    
+    for i = 1:numel(ivars)
+
+    indepvar = ivars{i};
+        
+        for kk = 1:num_sub
+
+        subject = subjects{kk};
+        
+        inpcfg = {'indepvar', indepvar, ...
+                   'freqband', freqband, ...
+                   'removeonset', 0, ...
+                   'word_selection', 'all', ...
+                   'epochtype', 'onset-lock-minoverlap', ...
+                   'epochlength', 0.5, ...
+                   'shift', [0 200 400 600], ...
+                   'savewhat', 'stat', ...
+                   'datadir', datadir, ...
+                   'savedir', savedir};
+        
+        qsubfeval('streams_dics', subject, inpcfg, ...
+                  'memreq', 1024^3*20,...
+                  'timreq', 60*40, ...
+                  'matlabcmd', 'matlab2016b', ...
+                  'display', 'yes');
+        
+        end
+    
+        
+    end
+    
+    
+end
+
+%% Computing trl indx
+
+[subjects, num_sub] = streams_util_subjectstring(11:28, {'s06', 's09'});
+
+ivars     = {'entropy', 'perplexity'};
+freqbands = {'theta', 'alpha', 'beta', 'high-beta', 'gamma', 'high-gamma'};
+
+datadir = '/project/3011085.04/streams/preproc/';
+savedir = '/project/3011085.04/streams/analysis/freqanalysis/source/subject3/onset_lock_minoverlap';
+
+% Subject loop
+for k = 1:numel(freqbands)
+    
+    freqband = freqbands{k};
+    
+    for i = 1:numel(ivars)
+
+    indepvar = ivars{i};
+        
+        for kk = 1:num_sub
+
+        subject = subjects{kk};
+        
+        inpcfg = {'indepvar', indepvar, ...
+                   'freqband', freqband, ...
+                   'removeonset', 0, ...
+                   'word_selection', 'all', ...
+                   'epochtype', 'onset-lock-minoverlap', ...
+                   'epochlength', 0.5, ...
+                   'shift', [0 200 400 600], ...
+                   'savewhat', 'stat', ...
+                   'datadir', datadir, ...
+                   'savedir', savedir};
+        
+        qsubfeval('streams_dics', subject, inpcfg, ...
+                  'memreq', 1024^3*20,...
+                  'timreq', 60*40, ...
                   'matlabcmd', 'matlab2016b', ...
                   'display', 'yes');
         
