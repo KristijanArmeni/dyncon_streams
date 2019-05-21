ae01ebcadc21e98d3130f480c0a72b26665f4e38
KristijanArmeni <kristijan.armeni@gmail.com>
2019-05-21 08:48:29 +0200

    added code for looping over onset locked analysis

--- matlab/pipeline_dics_groupcontrast.m
+++ matlab/pipeline_dics_groupcontrast.m
@@ -62,3 +62,36 @@ for i = 1:numel(ivars)
     
 end
 
+%% Analysis with onset locked timings
+
+datadir = '/project/3011085.04/streams/analysis/freqanalysis/source/subject3/onset_lock_minoverlap';
+savedir = '/project/3011085.04/streams/analysis/freqanalysis/source/group4-combinedfreq/onset_lock_minoverlap';
+
+ivars   = {'entropy', 'perplexity'};
+fois    = {'6', '10', '16', '25', '45', '75'};
+
+state = rng;
+
+for i = 1:numel(ivars)
+    
+    ivar = ivars{i};
+    
+     for ii = 1:numel(fois)
+        
+        foi = fois{ii};
+                        
+        opt = {'ivar', ivar, ...
+               'foi', foi, ...           % take all frequency bands
+               'setseed', state.State, ...
+               'savedir', savedir, ...
+               'datadir', datadir};
+
+        qsubfeval('streams_dics_groupcontrast_combined', opt, ...
+                  'memreq', 1024^3 * 5,...
+                  'timreq', 45*60, ...
+                  'matlabcmd', 'matlab2016b');
+
+     end
+    
+end
+
